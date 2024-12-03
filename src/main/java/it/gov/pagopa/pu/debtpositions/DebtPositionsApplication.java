e7b93d0f4acc96cbe2c9644df4ba2b9ee94e01dd
antonio.torre <antonio.torre@pagopa.it>
2024-12-03 19:34:04 +0100

    P4ADEV-1548 init-repo

--- src/main/java/it/gov/pagopa/pu/debtpositions/DebtPositionsApplication.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/DebtPositionsApplication.java
@@ -1,13 +1,13 @@
-package it.gov.pagopa.template;
+package it.gov.pagopa.pu.debtpositions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateApplication {
+public class DebtPositionsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TemplateApplication.class, args);
+		SpringApplication.run(DebtPositionsApplication.class, args);
 	}
 
 }
