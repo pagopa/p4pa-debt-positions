9e30c5f46ab29303e44bf607c8db985c3facba1d
Antonio Calò <antonio.calo@nttdata.com>
2024-12-23 13:41:14 +0100

    feat: P4ADEV-1773 add PersonalDataDAO and Citizen DataSource (#4)
    Co-authored-by: antonio.torre <antonio.torre@pagopa.it>

---
+++ src/main/java/it/gov/pagopa/pu/debtpositions/config/DebtPositionDataSourceConfig.java
@@ -0,0 +1,61 @@
+package it.gov.pagopa.pu.debtpositions.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+  entityManagerFactoryRef = "emfDebtPosition",
+  transactionManagerRef = "tmDebtPosition",
+  basePackages = {"it.gov.pagopa.pu.debtpositions.repository"}
+)
+public class DebtPositionDataSourceConfig {
+
+  @Primary
+  @Bean(name="dsDebtPosition")
+  @ConfigurationProperties("spring.datasource.debt-position")
+  public DataSource debtPositionDataSource()  {
+    return DataSourceBuilder.create().build();
+  }
+
+  @Primary
+  @Bean(name = "emfDebtPosition")
+  public LocalContainerEntityManagerFactoryBean debtPositionEntityManagerFactory(
+    @Qualifier("dsDebtPosition") DataSource dataSource,
+    EntityManagerFactoryBuilder builder) {
+
+    return builder.dataSource(dataSource)
+      .packages("it.gov.pagopa.pu.debtpositions.model")
+      .properties(Map.of(
+        "hibernate.physical_naming_strategy", CamelCaseToUnderscoresNamingStrategy.class.getName(),
+        "hibernate.implicit_naming_strategy", SpringImplicitNamingStrategy.class.getName()
+      ))
+      .persistenceUnit("debtPosition")
+      .build();
+  }
+
+  @Primary
+  @Bean(name = "tmDebtPosition")
+  public PlatformTransactionManager debtPositionTransactionManager(
+    @Qualifier("emfDebtPosition") EntityManagerFactory debtPositionEntityManagerFactory) {
+
+    return new JpaTransactionManager(debtPositionEntityManagerFactory);
+  }
+}
