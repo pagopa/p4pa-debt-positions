7dd7493c5f60aa2a071fdee3af1011e78864a65b
antonio.torre <antonio.torre@pagopa.it>
2025-01-08 20:10:21 +0100

    renamed crud operations

--- src/main/java/it/gov/pagopa/pu/debtpositions/config/RepositoryRestCustomConfiguration.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/config/RepositoryRestCustomConfiguration.java
@@ -2,6 +2,8 @@ package it.gov.pagopa.pu.debtpositions.config;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
+import org.apache.commons.lang3.StringUtils;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -11,19 +13,34 @@ import java.util.Set;
 @Configuration
 public class RepositoryRestCustomConfiguration {
 
-  private EntityManager entityManager;
+  private final EntityManager entityManager;
 
-  public RepositoryRestCustomConfiguration(EntityManager entityManager){
+  public RepositoryRestCustomConfiguration(EntityManager entityManager) {
     this.entityManager = entityManager;
   }
+
   @Bean
   public RepositoryRestConfigurer repositoryRestConfigurer() {
-    return RepositoryRestConfigurer.withConfig( config -> {
+    return RepositoryRestConfigurer.withConfig(config -> {
       Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
       config.exposeIdsFor(entities.stream()
         .map(EntityType::getJavaType)
         .toArray(Class[]::new));
-    } );
+    });
   }
 
+  @Bean
+  public OpenApiCustomizer operationIdCustomizer() {
+    return openApi -> openApi.getPaths().entrySet().stream()
+      .filter(e -> e.getKey().startsWith("/crud/"))
+      .forEach(entry -> {
+        String[] paths = entry.getKey().split("/");
+        entry.getValue().readOperations().forEach(operation -> operation.setOperationId(
+          "crud-" +
+            StringUtils.firstNonEmpty(
+              operation.getDescription(),
+              paths[2] + "-" + paths[paths.length - 1]
+            )));
+      });
+  }
 }
