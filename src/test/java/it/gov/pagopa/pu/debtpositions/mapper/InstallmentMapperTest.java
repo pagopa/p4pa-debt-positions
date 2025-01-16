925fe19cc68e0eb93f6e10a9c6947c053dff5406
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2025-01-16 09:48:31 +0100

    fix tests

--- src/test/java/it/gov/pagopa/pu/debtpositions/mapper/InstallmentMapperTest.java
+++ src/test/java/it/gov/pagopa/pu/debtpositions/mapper/InstallmentMapperTest.java
@@ -2,22 +2,43 @@ package it.gov.pagopa.pu.debtpositions.mapper;
 
 import it.gov.pagopa.pu.debtpositions.dto.Installment;
 import it.gov.pagopa.pu.debtpositions.dto.generated.InstallmentDTO;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static it.gov.pagopa.pu.debtpositions.util.TestUtils.checkNotNullFields;
+import static it.gov.pagopa.pu.debtpositions.util.faker.PersonFaker.buildPerson;
+import static it.gov.pagopa.pu.debtpositions.util.faker.PersonFaker.buildPersonDTO;
+import static it.gov.pagopa.pu.debtpositions.util.faker.TransferFaker.buildTransfer;
+import static it.gov.pagopa.pu.debtpositions.util.faker.TransferFaker.buildTransferDTO;
 import static org.junit.jupiter.api.Assertions.*;
 import static it.gov.pagopa.pu.debtpositions.util.faker.InstallmentFaker.*;
 
+@ExtendWith(MockitoExtension.class)
 class InstallmentMapperTest {
 
-  private final PersonMapper personMapper = new PersonMapper();
-  private final TransferMapper transferMapper = new TransferMapper();
-  private final InstallmentMapper installmentMapper = new InstallmentMapper(personMapper, transferMapper);
+  @Mock
+  private final PersonMapper personMapperMock = new PersonMapper();
+  @Mock
+  private final TransferMapper transferMapperMock = new TransferMapper();
+
+  private InstallmentMapper installmentMapper;
+
+  @BeforeEach
+  void setUp(){
+    installmentMapper = new InstallmentMapper(personMapperMock, transferMapperMock);
+  }
 
   @Test
   void givenValidInstallmentDTO_WhenMapToModel_ThenReturnInstallment() {
     Installment installmentExpected = buildInstallmentNoUpdate();
     InstallmentDTO installmentDTO = buildInstallmentDTO();
+
+    Mockito.when(personMapperMock.mapToModel(buildPersonDTO())).thenReturn(buildPerson());
+    Mockito.when(transferMapperMock.mapToModel(buildTransferDTO())).thenReturn(buildTransfer());
 
     Installment result = installmentMapper.mapToModel(installmentDTO);
 
