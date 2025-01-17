8312b8475f4cf7f76113cdd7daefa819f3b5aac0
antonio.torre <antonio.torre@pagopa.it>
2025-01-17 20:09:55 +0100

    disabling errorMvc

--- src/main/java/it/gov/pagopa/pu/debtpositions/DebtPositionsApplication.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/DebtPositionsApplication.java
@@ -2,8 +2,9 @@ package it.gov.pagopa.pu.debtpositions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class DebtPositionsApplication {
 
 	public static void main(String[] args) {
