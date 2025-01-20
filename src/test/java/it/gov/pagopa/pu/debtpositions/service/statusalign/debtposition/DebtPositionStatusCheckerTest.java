7c7118b3efb569767cc318a59cc478740bbd71d8
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2025-01-20 13:09:36 +0100

    P4ADEV-1744-add-api-finalize-sync-status

--- src/test/java/it/gov/pagopa/pu/debtpositions/service/statusalign/debtposition/DebtPositionStatusCheckerTest.java
+++ src/test/java/it/gov/pagopa/pu/debtpositions/service/statusalign/debtposition/DebtPositionStatusCheckerTest.java
@@ -20,6 +20,9 @@ class DebtPositionStatusCheckerTest {
     checker = new DebtPositionStatusChecker();
   }
 
+  /**
+   * Test if the status is TO_SYNC when at least one PaymentOption has status TO_SYNC.
+   */
   @Test
   void testDetermineDebtPositionStatus_ToSync() {
     List<PaymentOptionStatus> paymentOptionStatusList = List.of(PaymentOptionStatus.TO_SYNC, PaymentOptionStatus.PAID);
@@ -27,6 +30,9 @@ class DebtPositionStatusCheckerTest {
     assertEquals(DebtPositionStatus.TO_SYNC, result);
   }
 
+  /**
+   * Test if the status is PARTIALLY_PAID when there is at least one PAID and one UNPAID paymentOption.
+   */
   @Test
   void testDetermineDebtPositionStatus_PartiallyPaid() {
     List<PaymentOptionStatus> paymentOptionStatusList = List.of(PaymentOptionStatus.PAID, PaymentOptionStatus.UNPAID);
@@ -34,6 +40,19 @@ class DebtPositionStatusCheckerTest {
     assertEquals(DebtPositionStatus.PARTIALLY_PAID, result);
   }
 
+  /**
+   * Test if the status is PARTIALLY_PAID when there is at least one PAID and one EXPIRED paymentOption.
+   */
+  @Test
+  void testDeterminePaymentOptionStatus_PartiallyPaid2() {
+    List<PaymentOptionStatus> paymentOptionStatusList = List.of(PaymentOptionStatus.PAID, PaymentOptionStatus.EXPIRED);
+    DebtPositionStatus result = checker.determineDebtPositionStatus(paymentOptionStatusList);
+    assertEquals(DebtPositionStatus.PARTIALLY_PAID, result);
+  }
+
+  /**
+   * Test if the status is UNPAID when all paymentOptions are UNPAID.
+   */
   @Test
   void testDetermineDebtPositionStatus_Unpaid() {
     List<PaymentOptionStatus> paymentOptionStatusList = List.of(PaymentOptionStatus.UNPAID, PaymentOptionStatus.UNPAID);
@@ -41,6 +60,19 @@ class DebtPositionStatusCheckerTest {
     assertEquals(DebtPositionStatus.UNPAID, result);
   }
 
+  /**
+   * Test if the status is UNPAID when all paymentOptions are CANCELLED, with at least one UNPAID.
+   */
+  @Test
+  void testDeterminePaymentOptionStatus_Unpaid2() {
+    List<PaymentOptionStatus> paymentOptionStatusList = List.of(PaymentOptionStatus.UNPAID, PaymentOptionStatus.CANCELLED, PaymentOptionStatus.CANCELLED);
+    DebtPositionStatus result = checker.determineDebtPositionStatus(paymentOptionStatusList);
+    assertEquals(DebtPositionStatus.UNPAID, result);
+  }
+
+  /**
+   * Test if the status is PAID when all paymentOptions are PAID.
+   */
   @Test
   void testDetermineDebtPositionStatus_Paid() {
     List<PaymentOptionStatus> paymentOptionStatusList = List.of(PaymentOptionStatus.PAID, PaymentOptionStatus.PAID);
@@ -48,6 +80,19 @@ class DebtPositionStatusCheckerTest {
     assertEquals(DebtPositionStatus.PAID, result);
   }
 
+  /**
+   * Test if the status is PAID when all paymentOptions are CANCELLED, with at least one PAID.
+   */
+  @Test
+  void testDeterminePaymentOptionStatus_Paid2() {
+    List<PaymentOptionStatus> paymentOptionStatusList = List.of(PaymentOptionStatus.PAID, PaymentOptionStatus.CANCELLED);
+    DebtPositionStatus result = checker.determineDebtPositionStatus(paymentOptionStatusList);
+    assertEquals(DebtPositionStatus.PAID, result);
+  }
+
+  /**
+   * Test if the status is REPORTED when all paymentOptions are CANCELLED, with at least one REPORTED.
+   */
   @Test
   void testDetermineDebtPositionStatus_Reported() {
     List<PaymentOptionStatus> paymentOptionStatusList = List.of(PaymentOptionStatus.REPORTED, PaymentOptionStatus.CANCELLED);
@@ -55,13 +100,39 @@ class DebtPositionStatusCheckerTest {
     assertEquals(DebtPositionStatus.REPORTED, result);
   }
 
+  /**
+   * Test if the status is REPORTED when all paymentOptions are REPORTED.
+   */
+  @Test
+  void testDeterminePaymentOptionStatus_Reported2() {
+    List<PaymentOptionStatus> paymentOptionStatusList = List.of(PaymentOptionStatus.REPORTED, PaymentOptionStatus.REPORTED);
+    DebtPositionStatus result = checker.determineDebtPositionStatus(paymentOptionStatusList);
+    assertEquals(DebtPositionStatus.REPORTED, result);
+  }
+
+  /**
+   * Test if the status is INVALID when all paymentOptions are INVALID.
+   */
   @Test
   void testDetermineDebtPositionStatus_Invalid() {
+    List<PaymentOptionStatus> paymentOptionStatusList = List.of(PaymentOptionStatus.INVALID, PaymentOptionStatus.INVALID);
+    DebtPositionStatus result = checker.determineDebtPositionStatus(paymentOptionStatusList);
+    assertEquals(DebtPositionStatus.INVALID, result);
+  }
+
+  /**
+   * Test if the status is INVALID when all paymentOptions are CANCELLED, with at least one INVALID.
+   */
+  @Test
+  void testDeterminePaymentOptionStatus_Invalid2() {
     List<PaymentOptionStatus> paymentOptionStatusList = List.of(PaymentOptionStatus.INVALID, PaymentOptionStatus.CANCELLED);
     DebtPositionStatus result = checker.determineDebtPositionStatus(paymentOptionStatusList);
     assertEquals(DebtPositionStatus.INVALID, result);
   }
 
+  /**
+   * Test if the status is CANCELLED when all paymentOptions are CANCELLED.
+   */
   @Test
   void testDetermineDebtPositionStatus_Cancelled() {
     List<PaymentOptionStatus> paymentOptionStatusList = List.of(PaymentOptionStatus.CANCELLED, PaymentOptionStatus.CANCELLED);
@@ -69,6 +140,9 @@ class DebtPositionStatusCheckerTest {
     assertEquals(DebtPositionStatus.CANCELLED, result);
   }
 
+  /**
+   * Test if the status is EXPIRED when all paymentOptions are EXPIRED.
+   */
   @Test
   void testDetermineDebtPositionStatus_Expired() {
     List<PaymentOptionStatus> paymentOptionStatusList = List.of(PaymentOptionStatus.EXPIRED, PaymentOptionStatus.EXPIRED);
@@ -76,6 +150,9 @@ class DebtPositionStatusCheckerTest {
     assertEquals(DebtPositionStatus.EXPIRED, result);
   }
 
+  /**
+   * Test if an exception is thrown when the list of paymentOptions is empty.
+   */
   @Test
   void testDetermineDebtPositionStatus_InvalidStatus() {
     List<PaymentOptionStatus> paymentOptionStatusList = List.of();
--- src/test/java/it/gov/pagopa/pu/debtpositions/service/statusalign/paymentoption/PaymentOptionStatusCheckerTest.java
+++ src/test/java/it/gov/pagopa/pu/debtpositions/service/statusalign/paymentoption/PaymentOptionStatusCheckerTest.java
@@ -20,6 +20,9 @@ class PaymentOptionStatusCheckerTest {
     checker = new PaymentOptionStatusChecker();
   }
 
+  /**
+   * Test if the status is TO_SYNC when at least one Installment has status TO_SYNC.
+   */
   @Test
   void testDeterminePaymentOptionStatus_ToSync() {
     List<InstallmentStatus> installmentStatusList = List.of(InstallmentStatus.TO_SYNC, InstallmentStatus.PAID);
@@ -27,6 +30,9 @@ class PaymentOptionStatusCheckerTest {
     assertEquals(PaymentOptionStatus.TO_SYNC, result);
   }
 
+  /**
+   * Test if the status is PARTIALLY_PAID when there is at least one PAID and one UNPAID installment.
+   */
   @Test
   void testDeterminePaymentOptionStatus_PartiallyPaid() {
     List<InstallmentStatus> installmentStatusList = List.of(InstallmentStatus.PAID, InstallmentStatus.UNPAID);
@@ -34,6 +40,19 @@ class PaymentOptionStatusCheckerTest {
     assertEquals(PaymentOptionStatus.PARTIALLY_PAID, result);
   }
 
+  /**
+   * Test if the status is PARTIALLY_PAID when there is at least one PAID and one EXPIRED installment.
+   */
+  @Test
+  void testDeterminePaymentOptionStatus_PartiallyPaid2() {
+    List<InstallmentStatus> installmentStatusList = List.of(InstallmentStatus.PAID, InstallmentStatus.EXPIRED);
+    PaymentOptionStatus result = checker.determinePaymentOptionStatus(installmentStatusList);
+    assertEquals(PaymentOptionStatus.PARTIALLY_PAID, result);
+  }
+
+  /**
+   * Test if the status is UNPAID when all installments are UNPAID.
+   */
   @Test
   void testDeterminePaymentOptionStatus_Unpaid() {
     List<InstallmentStatus> installmentStatusList = List.of(InstallmentStatus.UNPAID, InstallmentStatus.UNPAID);
@@ -41,6 +60,19 @@ class PaymentOptionStatusCheckerTest {
     assertEquals(PaymentOptionStatus.UNPAID, result);
   }
 
+  /**
+   * Test if the status is UNPAID when all installments are CANCELLED, with at least one UNPAID.
+   */
+  @Test
+  void testDeterminePaymentOptionStatus_Unpaid2() {
+    List<InstallmentStatus> installmentStatusList = List.of(InstallmentStatus.UNPAID, InstallmentStatus.CANCELLED, InstallmentStatus.CANCELLED);
+    PaymentOptionStatus result = checker.determinePaymentOptionStatus(installmentStatusList);
+    assertEquals(PaymentOptionStatus.UNPAID, result);
+  }
+
+  /**
+   * Test if the status is PAID when all installments are PAID.
+   */
   @Test
   void testDeterminePaymentOptionStatus_Paid() {
     List<InstallmentStatus> installmentStatusList = List.of(InstallmentStatus.PAID, InstallmentStatus.PAID);
@@ -48,6 +80,19 @@ class PaymentOptionStatusCheckerTest {
     assertEquals(PaymentOptionStatus.PAID, result);
   }
 
+  /**
+   * Test if the status is PAID when all installments are CANCELLED, with at least one PAID.
+   */
+  @Test
+  void testDeterminePaymentOptionStatus_Paid2() {
+    List<InstallmentStatus> installmentStatusList = List.of(InstallmentStatus.PAID, InstallmentStatus.CANCELLED);
+    PaymentOptionStatus result = checker.determinePaymentOptionStatus(installmentStatusList);
+    assertEquals(PaymentOptionStatus.PAID, result);
+  }
+
+  /**
+   * Test if the status is REPORTED when all installments are CANCELLED, with at least one REPORTED.
+   */
   @Test
   void testDeterminePaymentOptionStatus_Reported() {
     List<InstallmentStatus> installmentStatusList = List.of(InstallmentStatus.REPORTED, InstallmentStatus.CANCELLED);
@@ -55,13 +100,39 @@ class PaymentOptionStatusCheckerTest {
     assertEquals(PaymentOptionStatus.REPORTED, result);
   }
 
+  /**
+   * Test if the status is REPORTED when all installments are REPORTED.
+   */
+  @Test
+  void testDeterminePaymentOptionStatus_Reported2() {
+    List<InstallmentStatus> installmentStatusList = List.of(InstallmentStatus.REPORTED, InstallmentStatus.REPORTED);
+    PaymentOptionStatus result = checker.determinePaymentOptionStatus(installmentStatusList);
+    assertEquals(PaymentOptionStatus.REPORTED, result);
+  }
+
+  /**
+   * Test if the status is INVALID when all installments are INVALID.
+   */
   @Test
   void testDeterminePaymentOptionStatus_Invalid() {
+    List<InstallmentStatus> installmentStatusList = List.of(InstallmentStatus.INVALID, InstallmentStatus.INVALID);
+    PaymentOptionStatus result = checker.determinePaymentOptionStatus(installmentStatusList);
+    assertEquals(PaymentOptionStatus.INVALID, result);
+  }
+
+  /**
+   * Test if the status is INVALID when all installments are CANCELLED, with at least one INVALID.
+   */
+  @Test
+  void testDeterminePaymentOptionStatus_Invalid2() {
     List<InstallmentStatus> installmentStatusList = List.of(InstallmentStatus.INVALID, InstallmentStatus.CANCELLED);
     PaymentOptionStatus result = checker.determinePaymentOptionStatus(installmentStatusList);
     assertEquals(PaymentOptionStatus.INVALID, result);
   }
 
+  /**
+   * Test if the status is CANCELLED when all installments are CANCELLED.
+   */
   @Test
   void testDeterminePaymentOptionStatus_Cancelled() {
     List<InstallmentStatus> installmentStatusList = List.of(InstallmentStatus.CANCELLED, InstallmentStatus.CANCELLED);
@@ -69,6 +140,9 @@ class PaymentOptionStatusCheckerTest {
     assertEquals(PaymentOptionStatus.CANCELLED, result);
   }
 
+  /**
+   * Test if the status is EXPIRED when all installments are EXPIRED.
+   */
   @Test
   void testDeterminePaymentOptionStatus_Expired() {
     List<InstallmentStatus> installmentStatusList = List.of(InstallmentStatus.EXPIRED, InstallmentStatus.EXPIRED);
@@ -76,6 +150,9 @@ class PaymentOptionStatusCheckerTest {
     assertEquals(PaymentOptionStatus.EXPIRED, result);
   }
 
+  /**
+   * Test if an exception is thrown when the list of installments is empty.
+   */
   @Test
   void testDeterminePaymentOptionStatus_InvalidStatus() {
     List<InstallmentStatus> installmentStatusList = List.of();
