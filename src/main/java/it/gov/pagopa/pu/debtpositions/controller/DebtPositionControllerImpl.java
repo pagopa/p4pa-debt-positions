5f8267f95e8cafe18abb45c532b8e470dceef883
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2025-01-20 16:34:00 +0100

    P4ADEV-1668 fix test

--- src/main/java/it/gov/pagopa/pu/debtpositions/controller/DebtPositionControllerImpl.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/controller/DebtPositionControllerImpl.java
@@ -1,14 +1,9 @@
 package it.gov.pagopa.pu.debtpositions.controller;
 
 import it.gov.pagopa.pu.debtpositions.controller.generated.DebtPositionApi;
-import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionDTO;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DebtPositionControllerImpl implements DebtPositionApi {
-  @Override
-  public ResponseEntity<DebtPositionDTO> createDebtPosition(DebtPositionDTO debtPositionDTO, Boolean massive) {
-    return DebtPositionApi.super.createDebtPosition(debtPositionDTO, massive);
-  }
+
 }
