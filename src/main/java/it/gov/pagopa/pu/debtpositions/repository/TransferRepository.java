578b4cce3dc6aec1feb7d5eb2812f705084a8ece
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2025-01-22 12:06:48 +0100

    [P4ADEV-1985] created query findBySemanticKey on transfer

--- src/main/java/it/gov/pagopa/pu/debtpositions/repository/TransferRepository.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/TransferRepository.java
@@ -2,9 +2,20 @@ package it.gov.pagopa.pu.debtpositions.repository;
 
 import it.gov.pagopa.pu.debtpositions.model.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Optional;
 
 @RepositoryRestResource(path = "transfers")
 public interface TransferRepository extends JpaRepository<Transfer,Long> {
 
+  @Query(value = "SELECT t.* from debt_positions.transfer t " +
+    "JOIN organizations.organization o ON t.org_fiscal_code = o.org_fiscal_code " +
+    "JOIN debt_positions.installment i ON t.installment_id = i.installment_id " +
+    "WHERE o.organization_id = :orgId AND " +
+    "i.iuv = :iuv AND " +
+    "i.iur = :iur AND " +
+    "t.transfer_index = :transferIndex", nativeQuery = true)
+  Optional<Transfer> findBySemanticKey(Long orgId, String iuv, String iur, Long transferIndex);
 }
