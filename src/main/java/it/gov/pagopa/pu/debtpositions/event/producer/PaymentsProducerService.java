32218e5b7747482f35e7bd9ef076a6631586ea0e
antonio.torre <antonio.torre@pagopa.it>
2025-01-22 17:29:18 +0100

    P4ADEV-2009 payments event producer

--- src/main/java/it/gov/pagopa/pu/debtpositions/event/producer/PaymentsProducerService.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/event/producer/PaymentsProducerService.java
@@ -5,16 +5,12 @@ import it.gov.pagopa.pu.debtpositions.event.producer.dto.PaymentEventDTO;
 import it.gov.pagopa.pu.debtpositions.event.producer.enums.PaymentEventType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.function.StreamBridge;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
-import java.util.function.Supplier;
-
 @Component
+@SuppressWarnings("squid:java:S125") // suppressing commented code warning
 public class PaymentsProducerService {
 
     @Value("${spring.cloud.stream.bindings.paymentsProducer-out-0.binder}")
@@ -26,6 +22,8 @@ public class PaymentsProducerService {
         this.streamBridge = streamBridge;
     }
 
+    /*
+    Producer not connected on startup, but just on-demand
     @Configuration
     static class PaymentsProducerConfig {
         @Bean
@@ -33,6 +31,7 @@ public class PaymentsProducerService {
             return () -> null;
         }
     }
+    */
 
     public void notifyPaymentsEvent(DebtPositionDTO debtPosition, PaymentEventType event){
         streamBridge.send("paymentsProducer-out-0", binder,
