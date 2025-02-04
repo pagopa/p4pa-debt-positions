5f90ad45f92ce7c6a9c437f96b961ea18c56edbf
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2025-02-04 16:32:27 +0100

    P4ADEV-1725 remove unused class

--- src/main/java/it/gov/pagopa/pu/debtpositions/service/DebtPositionTypeOrgService.java
+++
@@ -1,7 +0,0 @@
-package it.gov.pagopa.pu.debtpositions.service;
-
-import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionTypeOrgDTO;
-
-public interface DebtPositionTypeOrgService {
-  DebtPositionTypeOrgDTO getDebtPositionTypeOrgByOrganizationIdAndCode(Long organizationId, String code);
-}
