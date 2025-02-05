366462dabe76b3d0acb852b02bb717ab765ce1a0
ElisKina-dev <elis.kina@nttdata.com>
2025-02-05 10:50:12 +0100

    Receipt Custom Repository

--- src/main/java/it/gov/pagopa/pu/debtpositions/repository/view/debtpositiontype/DebtPositionTypeWithCountRepository.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/view/debtpositiontype/DebtPositionTypeWithCountRepository.java
@@ -1,17 +1,19 @@
-package it.gov.pagopa.pu.debtpositions.repository.debtpositiontype;
+package it.gov.pagopa.pu.debtpositions.repository.view.debtpositiontype;
 
-import it.gov.pagopa.pu.debtpositions.model.debtpositiontype.DebtPositionTypeWithCount;
+import it.gov.pagopa.pu.debtpositions.model.view.debtpositiontype.DebtPositionTypeWithCount;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "debt-position-types-with-count")
-public interface DebtPositionTypeWithCountRepository extends JpaRepository<DebtPositionTypeWithCount,Long> {
+public interface DebtPositionTypeWithCountRepository extends Repository<DebtPositionTypeWithCount, Long> {
+
   @Query(value = "SELECT d "
     + "FROM DebtPositionTypeWithCount d "
     + "WHERE d.brokerId = :brokerId ")
-    Page<DebtPositionTypeWithCount> findByBrokerId(@Param("brokerId") Long brokerId, Pageable pageable);
+  Page<DebtPositionTypeWithCount> findByBrokerId(@Param("brokerId") Long brokerId, Pageable pageable);
+
 }
