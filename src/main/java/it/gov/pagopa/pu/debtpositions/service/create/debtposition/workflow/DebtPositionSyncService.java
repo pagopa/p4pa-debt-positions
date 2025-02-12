2e9524f9f50365888bcc84eed34eef6b40221570
antonio.torre <antonio.torre@pagopa.it>
2025-02-12 16:21:02 +0100

    feat: Refactor workflow dispatch moved into workflow-hub

--- src/main/java/it/gov/pagopa/pu/debtpositions/service/create/debtposition/workflow/DebtPositionSyncService.java
+++
@@ -1,9 +0,0 @@
-package it.gov.pagopa.pu.debtpositions.service.create.debtposition.workflow;
-
-import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionDTO;
-import it.gov.pagopa.pu.workflowhub.dto.generated.WorkflowCreatedDTO;
-
-public interface DebtPositionSyncService {
-
-  WorkflowCreatedDTO invokeWorkFlow(DebtPositionDTO debtPositionDTO, String accessToken, Boolean massive);
-}
