6fb84c8911a0599f34b36e8995a5514b159ad8d7
MScarsella <marco.scarsella@emeal.nttdata.com>
2025-02-14 18:30:21 +0100

    feat: P4ADEV-2179-receipt-detail (#66)
    Co-authored-by: mscarsel <marco.scarsella@nttdata.com>

---
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/view/receipt/ReceiptDetailNoPIIViewRepository.java
@@ -0,0 +1,38 @@
+package it.gov.pagopa.pu.debtpositions.repository.view.receipt;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import it.gov.pagopa.pu.debtpositions.model.view.receipt.ReceiptDetailNoPIIView;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+@RepositoryRestResource(path = "receipts-detail-view")
+public interface ReceiptDetailNoPIIViewRepository extends Repository<ReceiptDetailNoPIIView, Long> {
+  @RestResource(exported = false)
+  @Query(value = "SELECT new ReceiptDetailNoPIIView("
+      + "r.receiptId as receiptId, "
+      + "i.iuv as iuv, "
+      + "r.paymentAmountCents as paymentAmountCents, "
+      + "i.remittanceInformation as remittanceInformation, "
+      + "dp.description as debtPositionDescription, "
+      + "i.personalDataId as debtorPersonalDataId, "
+      + "r.paymentDateTime as paymentDateTime, "
+      + "r.pspCompanyName as pspCompanyName, "
+      + "i.iud as iud, "
+      + "i.iur as iur "
+    + ") "
+    + "FROM ReceiptDetailNoPIIView r "
+    + "JOIN InstallmentNoPII i ON r.receiptId = i.receiptId "
+    + "JOIN PaymentOption po ON i.paymentOptionId = po.paymentOptionId "
+    + "JOIN DebtPosition dp ON po.debtPositionId = dp.debtPositionId "
+    + "JOIN DebtPositionTypeOrg dpto ON dp.debtPositionTypeOrgId = dpto.debtPositionTypeOrgId "
+    + "JOIN DebtPositionTypeOrgOperators dptoo ON dpto.debtPositionTypeOrgId = dptoo.debtPositionTypeOrgId "
+    + "WHERE r.receiptId = :receiptId "
+    + "AND dptoo.operatorExternalUserId = :operatorExternalUserId ")
+  Optional<ReceiptDetailNoPIIView> findReceiptDetailView(
+    @Parameter(required = true) @Param("receiptId") Long receiptId,
+    @Parameter(required = true) @Param("operatorExternalUserId") String operatorExternalUserId);
+}
--- src/main/java/it/gov/pagopa/pu/debtpositions/service/ReceiptServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/service/ReceiptServiceImpl.java
@@ -2,19 +2,24 @@ package it.gov.pagopa.pu.debtpositions.service;
 
 import it.gov.pagopa.pu.debtpositions.dto.Receipt;
 import it.gov.pagopa.pu.debtpositions.dto.generated.ReceiptDTO;
+import it.gov.pagopa.pu.debtpositions.dto.generated.ReceiptDetailDTO;
 import it.gov.pagopa.pu.debtpositions.dto.generated.ReceiptWithAdditionalNodeDataDTO;
 import it.gov.pagopa.pu.debtpositions.mapper.ReceiptMapper;
 import it.gov.pagopa.pu.debtpositions.repository.ReceiptPIIRepository;
+import it.gov.pagopa.pu.debtpositions.repository.view.receipt.ReceiptDetailPIIViewRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReceiptServiceImpl implements ReceiptService{
 
   private final ReceiptPIIRepository receiptPIIRepository;
+  private final ReceiptDetailPIIViewRepository receiptDetailPIIViewRepository;
   private final ReceiptMapper receiptMapper;
 
-  public ReceiptServiceImpl(ReceiptPIIRepository receiptPIIRepository, ReceiptMapper receiptMapper) {
+  public ReceiptServiceImpl(ReceiptPIIRepository receiptPIIRepository,
+    ReceiptDetailPIIViewRepository receiptDetailPIIViewRepository, ReceiptMapper receiptMapper) {
     this.receiptPIIRepository = receiptPIIRepository;
+    this.receiptDetailPIIViewRepository = receiptDetailPIIViewRepository;
     this.receiptMapper = receiptMapper;
   }
 
@@ -27,8 +32,8 @@ public class ReceiptServiceImpl implements ReceiptService{
   }
 
   @Override
-  public ReceiptDTO getReceiptDetail(Long receiptId) {
-    return receiptPIIRepository.getReceiptDetail(receiptId);
+  public ReceiptDetailDTO getReceiptDetail(Long receiptId, String operatorExternalUserId) {
+    return receiptDetailPIIViewRepository.getReceiptDetail(receiptId, operatorExternalUserId);
   }
 
 }
