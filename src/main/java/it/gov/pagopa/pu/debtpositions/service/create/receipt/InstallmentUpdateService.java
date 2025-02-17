e8714d03c8bbed378aa00aa710a6bd5ffaef812b
serdimic <sergio.dimichele@eng.it>
2025-02-17 18:02:32 +0100

    P4ADEV-2134 refactor of InstallmentUpdateService.updateInstallmentStatusOfDebtPosition

--- src/main/java/it/gov/pagopa/pu/debtpositions/service/create/receipt/InstallmentUpdateService.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/service/create/receipt/InstallmentUpdateService.java
@@ -6,12 +6,13 @@ import it.gov.pagopa.pu.debtpositions.exception.custom.NotFoundException;
 import it.gov.pagopa.pu.debtpositions.model.DebtPosition;
 import it.gov.pagopa.pu.debtpositions.model.InstallmentNoPII;
 import it.gov.pagopa.pu.debtpositions.model.InstallmentSyncStatus;
+import it.gov.pagopa.pu.debtpositions.model.PaymentOption;
 import it.gov.pagopa.pu.debtpositions.repository.DebtPositionRepository;
-import it.gov.pagopa.pu.organization.dto.generated.Broker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+import java.util.stream.Stream;
 
 @Service
 @Slf4j
@@ -29,35 +30,31 @@ public class InstallmentUpdateService {
   public DebtPosition updateInstallmentStatusOfDebtPosition(InstallmentNoPII installment, ReceiptDTO receiptDTO) {
     //retrieve debt position
     DebtPosition debtPosition = debtPositionRepository.findByInstallmentId(installment.getInstallmentId());
-    if(debtPosition == null) {
+    if (debtPosition == null) {
       throw new NotFoundException("debt position not found for installment " + installment.getInstallmentId());
     }
 
-    //update installment status
-    boolean[] primaryInstallmentFound = {false};
-    debtPosition.getPaymentOptions().forEach(paymentOption -> {
-      if (paymentOption.getPaymentOptionId().equals(installment.getPaymentOptionId())) {
-        // current payment option: find this installment
-        paymentOption.getInstallments().stream()
-          .filter(anInstallment -> anInstallment.getInstallmentId().equals(installment.getInstallmentId()))
-          .findAny().ifPresent(primaryInstallment -> {
-            // set status of the found installment to PAID and link it to the receipt
-            log.info("Installment [{}] found for receipt [{}]", primaryInstallment.getInstallmentId(), receiptDTO.getReceiptId());
-            primaryInstallmentFound[0] = true;
-            updateInstallmentFields(primaryInstallment, InstallmentStatus.PAID, receiptDTO.getReceiptId());
-          });
-      } else {
-        // change status of every other installment of different payment options of this debt position to INVALID
-        paymentOption.getInstallments().forEach(anInstallment -> {
-          if (NOT_PAID.contains(anInstallment.getStatus())) {
-            updateInstallmentFields(anInstallment, InstallmentStatus.INVALID, null);
-          }
-        });
-      }
-    });
-    if(!primaryInstallmentFound[0]) {
-      throw new NotFoundException("primary installment not found " + installment.getInstallmentId() + " on debt position " + debtPosition.getDebtPositionId());
-    }
+    debtPosition.getPaymentOptions().stream()
+      .flatMap(paymentOption -> {
+        if (paymentOption.getPaymentOptionId().equals(installment.getPaymentOptionId())) {
+          // current payment option: find this installment
+          return paymentOption.getInstallments().stream();
+        } else {
+          // change status of every other installment of different payment options of this debt position to INVALID
+          invalidOtherPaymentOptions(paymentOption);
+          return Stream.empty();
+        }
+      })
+      .filter(anInstallment -> anInstallment.getInstallmentId().equals(installment.getInstallmentId()))
+      .toList().stream() // we need to force evaluation of the entire stream
+      .findFirst() // we are sure that we may not have more than one element
+      .ifPresentOrElse(paidInstallment -> {
+        // set status of the found installment to PAID and link it to the receipt
+        log.info("Installment [{}] found for receipt [{}]", paidInstallment.getInstallmentId(), receiptDTO.getReceiptId());
+        updateInstallmentFields(paidInstallment, InstallmentStatus.PAID, receiptDTO.getReceiptId());
+      }, () -> {
+        throw new NotFoundException("primary installment not found " + installment.getInstallmentId() + " on debt position " + debtPosition.getDebtPositionId());
+      });
 
     return debtPosition;
   }
@@ -77,5 +74,13 @@ public class InstallmentUpdateService {
       .build()
     );
     installment.setStatus(InstallmentStatus.TO_SYNC);
+  }
+
+  private void invalidOtherPaymentOptions(PaymentOption paymentOption) {
+    paymentOption.getInstallments().forEach(anInstallment -> {
+      if (NOT_PAID.contains(anInstallment.getStatus())) {
+        updateInstallmentFields(anInstallment, InstallmentStatus.INVALID, null);
+      }
+    });
   }
 }
