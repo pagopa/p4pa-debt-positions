6ba9477996323cb0d56caeaec64a741f2355f9ae
ElisKina-dev <elis.kina@nttdata.com>
2025-02-27 11:03:08 +0100

    fix

--- src/main/java/it/gov/pagopa/pu/debtpositions/mapper/InstallmentDetailPIIViewMapper.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/mapper/InstallmentDetailPIIViewMapper.java
@@ -4,6 +4,7 @@ import it.gov.pagopa.pu.debtpositions.citizen.service.PersonalDataService;
 import it.gov.pagopa.pu.debtpositions.dto.InstallmentPIIDTO;
 import it.gov.pagopa.pu.debtpositions.dto.ReceiptPIIDTO;
 import it.gov.pagopa.pu.debtpositions.dto.generated.InstallmentDetailDTO;
+import it.gov.pagopa.pu.debtpositions.dto.generated.PersonDTO;
 import it.gov.pagopa.pu.debtpositions.model.view.installment.InstallmentDetailNoPIIView;
 import org.springframework.stereotype.Component;
 
@@ -23,8 +24,6 @@ public class InstallmentDetailPIIViewMapper {
   public InstallmentDetailDTO mapToInstallmentDetailDTO(InstallmentDetailNoPIIView installmentDetailNoPIIView) {
     InstallmentPIIDTO installmentPii = personalDataService.get(
       installmentDetailNoPIIView.getPersonalDataId(), InstallmentPIIDTO.class);
-    ReceiptPIIDTO receiptPii = personalDataService.get(
-      installmentDetailNoPIIView.getReceiptPersonalDataId(), ReceiptPIIDTO.class);
     return InstallmentDetailDTO.builder()
       .installmentId(installmentDetailNoPIIView.getInstallmentId())
       .receiptId(installmentDetailNoPIIView.getReceiptId())
@@ -38,11 +37,21 @@ public class InstallmentDetailPIIViewMapper {
       .debtPositionDescription(installmentDetailNoPIIView.getDebtPositionDescription())
       .debtPositionId(installmentDetailNoPIIView.getDebtPositionId())
       .paymentDateTime(installmentDetailNoPIIView.getPaymentDateTime())
-      .payer(receiptPii != null && receiptPii.getPayer() != null ? personMapper.mapToDto(receiptPii.getPayer()) : null)
+      .payer(getPayer(installmentDetailNoPIIView.getReceiptPersonalDataId()))
       .pspCompanyName(installmentDetailNoPIIView.getPspCompanyName())
       .iud(installmentDetailNoPIIView.getIud())
       .iur(installmentDetailNoPIIView.getIur())
       .build();
+  }
+
+  private PersonDTO getPayer(Long receiptPersonalDataId) {
+    if (receiptPersonalDataId != null) {
+      ReceiptPIIDTO receiptPii = personalDataService.get(receiptPersonalDataId, ReceiptPIIDTO.class);
+      if (receiptPii != null && receiptPii.getPayer() != null) {
+        return personMapper.mapToDto(receiptPii.getPayer());
+      }
+    }
+    return null;
   }
 
 }
--- src/main/java/it/gov/pagopa/pu/debtpositions/model/view/installment/InstallmentDetailNoPIIView.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/model/view/installment/InstallmentDetailNoPIIView.java
@@ -1,9 +1,7 @@
 package it.gov.pagopa.pu.debtpositions.model.view.installment;
 
 import it.gov.pagopa.pu.debtpositions.dto.generated.InstallmentStatus;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -24,6 +22,7 @@ public class InstallmentDetailNoPIIView implements Serializable {
   private Long installmentId;
   private Long receiptId;
   private Long paymentOptionId;
+  @Enumerated(EnumType.STRING)
   private InstallmentStatus status;
   private String iuv;
   private Long amountCents;
