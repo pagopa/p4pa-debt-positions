8603bfbe713b9ba9971342fdcc13d64ab51a0671
ElisKina-dev <144669238+ElisKina-dev@users.noreply.github.com>
2025-02-27 12:14:17 +0100

    feat: P4ADEV-2184 API getInstallmentDetail (#87)

---
+++ src/main/java/it/gov/pagopa/pu/debtpositions/model/view/installment/InstallmentDetailNoPIIView.java
@@ -0,0 +1,40 @@
+package it.gov.pagopa.pu.debtpositions.model.view.installment;
+
+import it.gov.pagopa.pu.debtpositions.dto.generated.InstallmentStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "installment")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class InstallmentDetailNoPIIView implements Serializable {
+
+  @Id
+  private Long installmentId;
+  private Long receiptId;
+  private Long paymentOptionId;
+  @Enumerated(EnumType.STRING)
+  private InstallmentStatus status;
+  private String iuv;
+  private Long amountCents;
+  private OffsetDateTime dueDate;
+  private Long personalDataId;
+  private String debtPositionTypeOrgDescription;
+  private String debtPositionDescription;
+  private Long debtPositionId;
+  private OffsetDateTime paymentDateTime;
+  private Long receiptPersonalDataId;
+  private String pspCompanyName;
+  private String iud;
+  private String iur;
+
+}
---
+++ src/test/java/it/gov/pagopa/pu/debtpositions/repository/InstallmentDetailPIIViewRepositoryImplTest.java
@@ -0,0 +1,71 @@
+package it.gov.pagopa.pu.debtpositions.repository;
+
+import it.gov.pagopa.pu.debtpositions.dto.generated.InstallmentDetailDTO;
+import it.gov.pagopa.pu.debtpositions.exception.custom.NotFoundException;
+import it.gov.pagopa.pu.debtpositions.mapper.InstallmentDetailPIIViewMapper;
+import it.gov.pagopa.pu.debtpositions.model.view.installment.InstallmentDetailNoPIIView;
+import it.gov.pagopa.pu.debtpositions.repository.view.installment.InstallmentDetailNoPIIViewRepository;
+import it.gov.pagopa.pu.debtpositions.repository.view.installment.InstallmentDetailPIIViewRepository;
+import it.gov.pagopa.pu.debtpositions.repository.view.installment.InstallmentDetailPIIViewRepositoryImpl;
+import it.gov.pagopa.pu.debtpositions.util.TestUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.co.jemos.podam.api.PodamFactory;
+
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+class InstallmentDetailPIIViewRepositoryImplTest {
+
+  @Mock
+  private InstallmentDetailNoPIIViewRepository installmentDetailNoPIIViewRepositoryMock;
+  @Mock
+  private InstallmentDetailPIIViewMapper installmentDetailPIIViewMapperMock;
+
+  private InstallmentDetailPIIViewRepository installmentDetailPIIViewRepository;
+  private final PodamFactory podamFactory = TestUtils.getPodamFactory();
+
+  @BeforeEach
+  void init() {
+    installmentDetailPIIViewRepository = new InstallmentDetailPIIViewRepositoryImpl(
+      installmentDetailNoPIIViewRepositoryMock, installmentDetailPIIViewMapperMock);
+  }
+
+  @Test
+  void givenExistingInstallmentWhenGetInstallmentDetailThenOk() {
+    Long installmentId = 1L;
+    String operatorExternalUserId = "operatorExternalUserId";
+    InstallmentDetailNoPIIView installmentDetailNoPIIView = podamFactory.manufacturePojo(
+      InstallmentDetailNoPIIView.class);
+    InstallmentDetailDTO installmentDetail = podamFactory.manufacturePojo(InstallmentDetailDTO.class);
+
+    Mockito.when(installmentDetailNoPIIViewRepositoryMock.findInstallmentDetailView(installmentId, operatorExternalUserId)).thenReturn(
+      Optional.of(installmentDetailNoPIIView));
+    Mockito.when(installmentDetailPIIViewMapperMock.mapToInstallmentDetailDTO(installmentDetailNoPIIView)).thenReturn(installmentDetail);
+
+    InstallmentDetailDTO result = installmentDetailPIIViewRepository.getInstallmentDetail(installmentId, operatorExternalUserId);
+
+    Assertions.assertEquals(installmentDetail, result);
+    Mockito.verify(installmentDetailNoPIIViewRepositoryMock).findInstallmentDetailView(installmentId, operatorExternalUserId);
+    Mockito.verify(installmentDetailPIIViewMapperMock).mapToInstallmentDetailDTO(installmentDetailNoPIIView);
+  }
+
+  @Test
+  void givenNonExistingInstallmentWhenFindInstallmentThenNotFoundException() {
+    Long installmentId = 1L;
+    String operatorExternalUserId = "operatorExternalUserId";
+
+    Mockito.when(installmentDetailNoPIIViewRepositoryMock.findInstallmentDetailView(installmentId, operatorExternalUserId))
+      .thenReturn(Optional.empty());
+
+    Assertions.assertThrows(NotFoundException.class, () -> installmentDetailPIIViewRepository.getInstallmentDetail(installmentId, operatorExternalUserId));
+
+    Mockito.verify(installmentDetailNoPIIViewRepositoryMock).findInstallmentDetailView(installmentId, operatorExternalUserId);
+    Mockito.verifyNoInteractions(installmentDetailPIIViewMapperMock);
+  }
+}
