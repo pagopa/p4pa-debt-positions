62029c6a6502def7e49351c593f245bd613713c2
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-03-03 09:25:21 +0100

    feat: Caching pii (#95)

---
+++ src/main/java/it/gov/pagopa/pu/debtpositions/config/CacheConfig.java
@@ -0,0 +1,52 @@
+package it.gov.pagopa.pu.debtpositions.config;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldNameConstants;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@ConfigurationProperties(prefix = "cache")
+@EnableCaching
+@Data
+@FieldNameConstants
+public class CacheConfig {
+
+    @NestedConfigurationProperty
+    private CacheConfigurationProperties pii;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CacheConfigurationProperties {
+        private long size;
+        private long expireIn;
+    }
+
+    @Bean
+    @Primary
+    public CacheManager localCacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.registerCustomCache(Fields.pii, buildCache(pii));
+        return cacheManager;
+    }
+
+    private Cache<Object, Object> buildCache(CacheConfigurationProperties cacheConfig) {
+        return Caffeine.newBuilder()
+                .maximumSize(cacheConfig.size)
+                .expireAfterAccess(cacheConfig.expireIn, TimeUnit.MINUTES)
+                .build();
+    }
+}
