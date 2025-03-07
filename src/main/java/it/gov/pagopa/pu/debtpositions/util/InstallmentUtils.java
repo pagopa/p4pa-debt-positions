f40b8fb1218e0b1f92d99106b9df33614632389b
domenicogi <domenico.giacobbe@eng.it>
2025-03-07 15:57:42 +0100

    Update src/main/java/it/gov/pagopa/pu/debtpositions/util/InstallmentUtils.java
    Co-authored-by: antonioT90 <34568575+antonioT90@users.noreply.github.com>

--- src/main/java/it/gov/pagopa/pu/debtpositions/util/InstallmentUtils.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/util/InstallmentUtils.java
@@ -10,7 +10,7 @@ public class InstallmentUtils {
   private InstallmentUtils() {
   }
 
-  public static final Set<String> TRANSITION_TO_SYNC_REQUIRED = Set.of(
+  private static final Set<String> TRANSITION_TO_SYNC_REQUIRED = Set.of(
     InstallmentStatus.DRAFT + "|" + InstallmentStatus.UNPAID,
     InstallmentStatus.UNPAID + "|" + InstallmentStatus.INVALID,
     InstallmentStatus.UNPAID + "|" + InstallmentStatus.CANCELLED);
