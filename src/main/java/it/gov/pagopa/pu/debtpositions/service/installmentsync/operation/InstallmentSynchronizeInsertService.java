266a7e7899b242e1af052e2c06774ba964af7c20
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2025-03-21 09:54:00 +0100

    fix check insert sync installment

--- src/main/java/it/gov/pagopa/pu/debtpositions/service/installmentsync/operation/InstallmentSynchronizeInsertService.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/service/installmentsync/operation/InstallmentSynchronizeInsertService.java
@@ -34,29 +34,41 @@ public class InstallmentSynchronizeInsertService extends BaseInstallmentSynchron
 
   public String syncInstallment(InstallmentSynchronizeDTO installmentSynchronizeDTO, DebtPositionDTO storedDebtPosition, WfExecutionParameters wfExecutionParameters, String accessToken, String operatorExternalUserId) {
     Pair<PaymentOptionDTO, InstallmentDTO> result = findInstallment(storedDebtPosition, installmentSynchronizeDTO);
-    PaymentOptionDTO storedPaymentOption = result == null ? null: result.getLeft();
-    InstallmentDTO storedInstallment = result == null ? null: result.getRight();
+    PaymentOptionDTO storedPaymentOption = result == null ? null : result.getLeft();
+    InstallmentDTO storedInstallment = result == null ? null : result.getRight();
 
-    if(isInstallmentAlreadyElaborated(storedInstallment, installmentSynchronizeDTO)){ return null;}
-    checkStatus(storedDebtPosition, storedPaymentOption, storedInstallment);
+    if (isInstallmentAlreadyElaborated(storedInstallment, installmentSynchronizeDTO)) {
+      return null;
+    }
+    checkStatus(storedDebtPosition, storedPaymentOption, storedInstallment, installmentSynchronizeDTO.getIngestionFlowFileId());
 
     Pair<DebtPositionDTO, InstallmentDTO> debtPositionApplied = installmentSynchronizeApplierService.apply(installmentSynchronizeDTO, storedDebtPosition, storedPaymentOption, storedInstallment, accessToken);
 
-    if(debtPositionApplied.getLeft().getDebtPositionId() == null) {
+    if (debtPositionApplied.getLeft().getDebtPositionId() == null) {
       return debtPositionCreationService.createDebtPosition(debtPositionApplied.getLeft(), wfExecutionParameters, accessToken, operatorExternalUserId).getRight();
     }
 
     return debtPositionAddInstallmentService.addInstallment(debtPositionApplied.getLeft(), List.of(debtPositionApplied.getRight()), wfExecutionParameters, accessToken, operatorExternalUserId).getRight();
   }
 
-  private void checkStatus(DebtPositionDTO storedDebtPosition, PaymentOptionDTO storedPaymentOption, InstallmentDTO storedInstallment) {
-    if(storedDebtPosition != null && !debtPositionStatusesValidForInsertion.contains(storedDebtPosition.getStatus())){
-      throw new ConflictErrorException(String.format("The installment cannot be created because the debt position with iupd %s is not in an allowed status: %s",
-        storedDebtPosition.getIupdOrg(), storedDebtPosition.getStatus()));
+  private void checkStatus(DebtPositionDTO storedDebtPosition, PaymentOptionDTO storedPaymentOption, InstallmentDTO storedInstallment, Long ingestionFlowFileId) {
+    if (storedDebtPosition != null) {
+      if (DebtPositionStatus.TO_SYNC.equals(storedDebtPosition.getStatus()) && !isDPToSyncAllowed(storedDebtPosition, ingestionFlowFileId)) {
+        throw new ConflictErrorException(String.format("The installment cannot be created because the debt position with iupd %s is in TO_SYNC status for a previous synchronization",
+          storedDebtPosition.getIupdOrg()));
+      } else if (!debtPositionStatusesValidForInsertion.contains(storedDebtPosition.getStatus())) {
+        throw new ConflictErrorException(String.format("The installment cannot be created because the debt position with iupd %s is not in an allowed status: %s",
+          storedDebtPosition.getIupdOrg(), storedDebtPosition.getStatus()));
+      }
     }
-    if(storedPaymentOption != null && !paymentOptionStatusesValidForInsertion.contains(storedPaymentOption.getStatus())) {
-      throw new ConflictErrorException(String.format("The installment cannot be created because the payment option with index %s is not in an allowed status: %s",
-        storedPaymentOption.getPaymentOptionIndex(), storedPaymentOption.getStatus()));
+    if (storedPaymentOption != null) {
+      if (PaymentOptionStatus.TO_SYNC.equals(storedPaymentOption.getStatus()) && !isPOToSyncAllowed(storedPaymentOption, ingestionFlowFileId)) {
+        throw new ConflictErrorException(String.format("The installment cannot be created because the payment option with index %s is in TO_SYNC status for a previous synchronization",
+          storedPaymentOption.getPaymentOptionIndex()));
+      } else if (!paymentOptionStatusesValidForInsertion.contains(storedPaymentOption.getStatus())) {
+        throw new ConflictErrorException(String.format("The installment cannot be created because the payment option with index %s is not in an allowed status: %s",
+          storedPaymentOption.getPaymentOptionIndex(), storedPaymentOption.getStatus()));
+      }
     }
     if (storedInstallment != null && !installmentStatusesValidForInsertion.contains(storedInstallment.getStatus())) {
       throw new ConflictErrorException(String.format("The installment with iud %s cannot be created because it already exists in a not modifiable status: %s",
@@ -64,5 +76,16 @@ public class InstallmentSynchronizeInsertService extends BaseInstallmentSynchron
     }
   }
 
+  private boolean isDPToSyncAllowed(DebtPositionDTO storedDebtPosition, Long ingestionFlowFileId) {
+    return storedDebtPosition.getPaymentOptions().stream().allMatch(
+      paymentOptionDTO -> paymentOptionDTO.getInstallments()
+        .stream().allMatch(installmentDTO -> installmentDTO.getIngestionFlowFileId().equals(ingestionFlowFileId))
+    );
+  }
+
+  private boolean isPOToSyncAllowed(PaymentOptionDTO storedPaymentOption, Long ingestionFlowFileId) {
+    return storedPaymentOption.getInstallments()
+      .stream().allMatch(installmentDTO -> installmentDTO.getIngestionFlowFileId().equals(ingestionFlowFileId));
+  }
 
 }
