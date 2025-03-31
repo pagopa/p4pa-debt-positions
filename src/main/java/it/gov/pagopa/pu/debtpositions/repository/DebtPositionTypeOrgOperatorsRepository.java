4de30f316939e9a9f34158702a811cb03849189c
pelliccm <matteo.pelliccione@nttdata.com>
2025-03-31 18:14:16 +0200

    [P4ADEV-2394] Organization findBy brokerId and optional orgName

--- src/main/java/it/gov/pagopa/pu/debtpositions/repository/DebtPositionTypeOrgOperatorsRepository.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/DebtPositionTypeOrgOperatorsRepository.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.pu.debtpositions.repository;
 
 import it.gov.pagopa.pu.debtpositions.model.DebtPositionTypeOrgOperators;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -8,4 +9,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public interface DebtPositionTypeOrgOperatorsRepository extends JpaRepository<DebtPositionTypeOrgOperators,Long>{
 
   DebtPositionTypeOrgOperators findByOperatorExternalUserId(String operatorExternalUserId);
+  List<DebtPositionTypeOrgOperators> findByDebtPositionTypeOrgId(Long debtPositionTypeOrgId);
 }
