3ac52720b7ab00a4288bf265ad8527a1e1db7004
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2025-03-31 15:45:50 +0200

    P4ADEV-2515 added ReceiptArchivingNoPIIView and mapper

---
+++ src/main/java/it/gov/pagopa/pu/debtpositions/mapper/PagedReceiptsArchivingViewMapper.java
@@ -0,0 +1,39 @@
+package it.gov.pagopa.pu.debtpositions.mapper;
+
+import it.gov.pagopa.pu.debtpositions.dto.generated.PagedReceiptsArchivingView;
+import it.gov.pagopa.pu.debtpositions.model.view.receipt.ReceiptArchivingNoPIIView;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+public class PagedReceiptsArchivingViewMapper {
+
+    private final ReceiptArchivingPIIMapping receiptArchivingPIIMapping;
+
+  public PagedReceiptsArchivingViewMapper(ReceiptArchivingPIIMapping receiptArchivingPIIMapping) {
+    this.receiptArchivingPIIMapping = receiptArchivingPIIMapping;
+  }
+
+
+  public PagedReceiptsArchivingView mapToPagedReceiptsArchivingView(Page<ReceiptArchivingNoPIIView> pagedReceiptArchivingViewNoPIIDTO){
+      PagedReceiptsArchivingView mappedPagedReceiptsArchivingView = new PagedReceiptsArchivingView();
+      if(pagedReceiptArchivingViewNoPIIDTO != null){
+        if (!pagedReceiptArchivingViewNoPIIDTO.getContent().isEmpty()){
+          mappedPagedReceiptsArchivingView.setContent(pagedReceiptArchivingViewNoPIIDTO.stream().map(receiptArchivingPIIMapping::map).toList());
+        }else {
+          mappedPagedReceiptsArchivingView.setContent(Collections.emptyList());
+        }
+
+        if(pagedReceiptArchivingViewNoPIIDTO.getPageable().isPaged()){
+          mappedPagedReceiptsArchivingView.setTotalPages((long) pagedReceiptArchivingViewNoPIIDTO.getTotalPages());
+          mappedPagedReceiptsArchivingView.setSize((long) pagedReceiptArchivingViewNoPIIDTO.getSize());
+          mappedPagedReceiptsArchivingView.setNumber((long) pagedReceiptArchivingViewNoPIIDTO.getNumber());
+          mappedPagedReceiptsArchivingView.setTotalElements(pagedReceiptArchivingViewNoPIIDTO.getTotalElements());
+        }
+      }
+      return mappedPagedReceiptsArchivingView;
+    }
+
+}
