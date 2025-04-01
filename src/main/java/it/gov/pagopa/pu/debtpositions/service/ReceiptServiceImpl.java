4259e2bd42db841f48a696cfb9719e3dd22ec92d
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2025-04-01 16:24:14 +0200

    P4ADEV-2517 added properties to application.yml and added repository and updated service

--- src/main/java/it/gov/pagopa/pu/debtpositions/service/ReceiptServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/service/ReceiptServiceImpl.java
@@ -1,16 +1,23 @@
 package it.gov.pagopa.pu.debtpositions.service;
 
+import it.gov.pagopa.pu.debtpositions.dto.generated.PagedReceiptsArchivingView;
 import it.gov.pagopa.pu.debtpositions.dto.generated.ReceiptDetailDTO;
+import it.gov.pagopa.pu.debtpositions.repository.view.receipt.ReceiptArchivingPIIViewRepository;
 import it.gov.pagopa.pu.debtpositions.repository.view.receipt.ReceiptDetailPIIViewRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
 
 @Service
 public class ReceiptServiceImpl implements ReceiptService {
 
   private final ReceiptDetailPIIViewRepository receiptDetailPIIViewRepository;
+  private final ReceiptArchivingPIIViewRepository receiptArchivingPIIViewRepository;
 
-  public ReceiptServiceImpl(ReceiptDetailPIIViewRepository receiptDetailPIIViewRepository) {
+  public ReceiptServiceImpl(ReceiptDetailPIIViewRepository receiptDetailPIIViewRepository, ReceiptArchivingPIIViewRepository receiptArchivingPIIViewRepository) {
     this.receiptDetailPIIViewRepository = receiptDetailPIIViewRepository;
+    this.receiptArchivingPIIViewRepository = receiptArchivingPIIViewRepository;
   }
 
   @Override
@@ -18,4 +25,8 @@ public class ReceiptServiceImpl implements ReceiptService {
     return receiptDetailPIIViewRepository.getReceiptDetail(receiptId, operatorExternalUserId);
   }
 
+  @Override
+  public PagedReceiptsArchivingView getPagedReceiptArchivingView(Long organizationId, String operatorExternalUserId, OffsetDateTime paymentDateFrom, OffsetDateTime paymentDateTo, Long debtPositionTypeOrgId, Pageable pageable) {
+    return receiptArchivingPIIViewRepository.getPagedReceiptsArchivingView(organizationId, operatorExternalUserId, paymentDateFrom, paymentDateTo, debtPositionTypeOrgId, pageable);
+  }
 }
