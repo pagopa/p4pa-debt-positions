1385249ae98f0b02c6f25c77ddfa5271524fc524
Giuseppe-LaManna <127106061+Giuseppe-LaManna@users.noreply.github.com>
2025-04-01 14:50:13 +0200

    chore: P4ADEV-2516 added receipt archiving query (#157)

---
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/view/receipt/ReceiptArchivingNoPIIViewRepository.java
@@ -0,0 +1,53 @@
+package it.gov.pagopa.pu.debtpositions.repository.view.receipt;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import it.gov.pagopa.pu.debtpositions.model.view.receipt.ReceiptArchivingNoPIIView;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.time.OffsetDateTime;
+
+@RepositoryRestResource(path = "receipt-archiving-export-view")
+public interface ReceiptArchivingNoPIIViewRepository extends Repository<ReceiptArchivingNoPIIView, Long> {
+    @RestResource(exported = false)
+    @Query("""
+    SELECT new ReceiptArchivingNoPIIView(
+      r.receiptId as receiptId,
+      r.paymentReceiptId as paymentReceiptId,
+      r.paymentDateTime as paymentDateTime,
+      r.creditorReferenceId as creditorReferenceId,
+      i.iuv as iuv,
+      t.remittanceInformation as remittanceInformation,
+      t.orgFiscalCode as orgFiscalCode,
+      i.personalDataId as installmentPersonalDataId
+    )
+    FROM ReceiptNoPII r
+    JOIN InstallmentNoPII i ON r.receiptId = i.receiptId
+    JOIN PaymentOption po ON i.paymentOptionId = po.paymentOptionId
+    JOIN DebtPosition dp ON po.debtPositionId = dp.debtPositionId
+    JOIN Transfer t ON i.installmentId = t.installmentId
+    JOIN DebtPositionTypeOrgOperators dptoo ON dp.debtPositionTypeOrgId = dptoo.debtPositionTypeOrgId
+    JOIN DebtPositionTypeOrg dpto ON dp.debtPositionTypeOrgId = dpto.debtPositionTypeOrgId
+    WHERE
+      (i.status = 'PAID' OR i.status = 'REPORTED')
+      AND dp.organizationId = :organizationId
+      AND r.paymentDateTime BETWEEN :paymentDateTimeFrom AND :paymentDateTimeTo
+      AND dptoo.operatorExternalUserId = :operatorExternalUserId
+      AND (:debtPositionTypeOrgId IS NULL OR dptoo.debtPositionTypeOrgId = :debtPositionTypeOrgId)
+      AND t.transferIndex = 1
+  """
+    )
+    Page<ReceiptArchivingNoPIIView> findReceiptArchivingViewNoPIIDTO(
+            @Parameter(required = true) @Param("organizationId") Long organizationId,
+            @Parameter(required = true) @Param("operatorExternalUserId") String operatorExternalUserId,
+            @Parameter(required = true) @Param("paymentDateTimeFrom") OffsetDateTime paymentDateTimeFrom,
+            @Parameter(required = true) @Param("paymentDateTimeTo") OffsetDateTime paymentDateTimeTo,
+            @Param("debtPositionTypeOrgId") Long debtPositionTypeOrgId,
+            Pageable pageable
+    );
+}
