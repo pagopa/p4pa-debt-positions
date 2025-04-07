788b2cd6246c4b6104d864aa83e177a215da2e77
ElisKina-dev <144669238+ElisKina-dev@users.noreply.github.com>
2025-04-07 17:10:49 +0200

    feat: P4ADEV-2538 find DebtPoistionTypeOrg by debtPositionTypeId (#171)

--- src/main/java/it/gov/pagopa/pu/debtpositions/repository/DebtPositionTypeOrgRepository.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/DebtPositionTypeOrgRepository.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.pu.debtpositions.repository;
 
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import it.gov.pagopa.pu.debtpositions.model.DebtPositionTypeOrg;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -25,6 +26,7 @@ public interface DebtPositionTypeOrgRepository extends JpaRepository<DebtPositio
   List<DebtPositionTypeOrg> findDebtPositionTypeOrgs(@Parameter(required = true) @Param("organizationId") Long organizationId,
                                                      @Parameter(required = true) @Param("operatorExternalUserId") String operatorExternalUserId);
 
+  List<DebtPositionTypeOrg> findByDebtPositionTypeId(@Parameter(required = true, schema = @Schema(type = "integer", format = "int64")) @Param("debtPositionTypeId") Long debtPositionTypeId);
 
   @Query("select dpto from InstallmentNoPII i " +
     "join PaymentOption po on i.paymentOptionId = po.paymentOptionId " +
