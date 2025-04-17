bdf787065e89173235a212ef17c2fee7b5ee6d84
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2025-04-17 09:38:36 -0300

    P4ADEV-2723-add-payment-option-entity-extended-controller

--- src/main/java/it/gov/pagopa/pu/debtpositions/repository/PaymentOptionRepository.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/PaymentOptionRepository.java
@@ -9,12 +9,14 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "payment-options")
 public interface PaymentOptionRepository extends JpaRepository<PaymentOption,Long> {
 
   @Transactional
   @Modifying
+  @RestResource(exported = false)
   @Query("UPDATE PaymentOption p SET p.status = :status WHERE p.paymentOptionId = :paymentOptionId")
   @ApiResponse(responseCode = "200", description = "Status updated successfully")
   void updateStatus(@Param("paymentOptionId") Long paymentOptionId, @Param("status") PaymentOptionStatus status);
