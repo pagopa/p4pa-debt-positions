d9c5c16f345dd130f2a4b2acd246e7618e0de6d9
ElisKina-dev <elis.kina@nttdata.com>
2025-05-12 14:08:49 +0200

    findAllByBrokerIdAndOrgType API

--- src/main/java/it/gov/pagopa/pu/debtpositions/repository/DebtPositionTypeRepository.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/DebtPositionTypeRepository.java
@@ -1,10 +1,18 @@
 package it.gov.pagopa.pu.debtpositions.repository;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import it.gov.pagopa.pu.debtpositions.model.DebtPositionType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(path = "debt-position-types")
 public interface DebtPositionTypeRepository extends JpaRepository<DebtPositionType,Long> {
 
+  List<DebtPositionType> findAllByBrokerIdAndOrgType(
+    @Parameter(required = true, schema = @Schema(type = "integer", format = "int64")) @Param("brokerId") Long brokerId,
+    @Parameter(required = true) @Param("orgType") String orgType);
 }
