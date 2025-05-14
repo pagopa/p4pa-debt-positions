90495bbc5f57e1edcbe575927ff387b7722c8c25
antonio.torre <antonio.torre@pagopa.it>
2025-05-14 15:40:18 +0200

    fix: CVE scan + libraries updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ repositories {
 
 dependencyManagement {
   imports {
-    mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0")
+    mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.1")
   }
 }
 
