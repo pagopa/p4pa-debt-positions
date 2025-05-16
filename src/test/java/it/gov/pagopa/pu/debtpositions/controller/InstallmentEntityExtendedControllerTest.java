d47eea8498fbc4d59f8715570a48acce0ed34aec
Antonio Calò <antonio.calo@nttdata.com>
2025-05-16 10:14:17 +0200

    feat: P4ADEV-2726 change UpdateIUN with debtPositionId in input (#218)

--- src/test/java/it/gov/pagopa/pu/debtpositions/controller/InstallmentEntityExtendedControllerTest.java
+++ src/test/java/it/gov/pagopa/pu/debtpositions/controller/InstallmentEntityExtendedControllerTest.java
@@ -75,13 +75,13 @@ class InstallmentEntityExtendedControllerTest {
   @Test
   void whenUpdateIunThenInvokeRepository() {
     // Given
-    Long installmentId = 1L;
+    Long debtPositionId = 1L;
     String iun = "IUN";
 
     // When
-    controller.updateIun(installmentId, iun);
+    controller.updateIunByDebtPositionId(debtPositionId, iun);
 
     // Then
-    verify(repositoryMock).updateIun(installmentId, iun);
+    verify(repositoryMock).updateIunByDebtPositionId(debtPositionId, iun);
   }
 }
