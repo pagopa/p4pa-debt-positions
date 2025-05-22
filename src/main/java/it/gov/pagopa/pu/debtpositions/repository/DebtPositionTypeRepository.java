eac9f90f6a794f9b6eaa4f9567bcbc0903a132f5
dgiacobbe <domenico.giacobbe@eng.it>
2025-05-23 00:10:11 +0200

    P4ADEV-2996 new api for debt position type

--- src/main/java/it/gov/pagopa/pu/debtpositions/repository/DebtPositionTypeRepository.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/DebtPositionTypeRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
+import java.util.Optional;
 
 @RepositoryRestResource(path = "debt-position-types")
 public interface DebtPositionTypeRepository extends JpaRepository<DebtPositionType,Long> {
@@ -15,4 +16,6 @@ public interface DebtPositionTypeRepository extends JpaRepository<DebtPositionTy
   List<DebtPositionType> findAllByBrokerIdAndOrgType(
     @Parameter(required = true, schema = @Schema(type = "integer", format = "int64")) @Param("brokerId") Long brokerId,
     @Parameter(required = true) @Param("orgType") String orgType);
+
+  Optional<DebtPositionType> findByCode(String code);
 }
