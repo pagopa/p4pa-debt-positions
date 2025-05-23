c10c6efcb5e4663717c014e8b46995fa9b4dba23
dgiacobbe <domenico.giacobbe@eng.it>
2025-05-23 10:46:38 +0200

    P4ADEV-2996 new api for debt position type

--- src/main/java/it/gov/pagopa/pu/debtpositions/repository/DebtPositionTypeRepository.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/DebtPositionTypeRepository.java
@@ -4,18 +4,27 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import it.gov.pagopa.pu.debtpositions.model.DebtPositionType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
 @RepositoryRestResource(path = "debt-position-types")
-public interface DebtPositionTypeRepository extends JpaRepository<DebtPositionType,Long> {
+public interface DebtPositionTypeRepository extends JpaRepository<DebtPositionType, Long> {
 
   List<DebtPositionType> findAllByBrokerIdAndOrgType(
     @Parameter(required = true, schema = @Schema(type = "integer", format = "int64")) @Param("brokerId") Long brokerId,
     @Parameter(required = true) @Param("orgType") String orgType);
 
-  List<DebtPositionType> findByCodeAndBrokerIdAndOrgTypeAndMacroAreaAndServiceTypeAndCollectingReasonAndTaxonomyCode
-    (String code, Long brokerId, String orgType, String macroArea, String serviceType, String collectingReason, String taxonomyCode);
+@Query("select dpt from DebtPositionType dpt " +
+  "where dpt.code = :code " +
+  "and dpt.brokerId = :brokerId " +
+  "and dpt.orgType = :orgType " +
+  "and dpt.macroArea = :macroArea " +
+  "and dpt.serviceType = :serviceType " +
+  "and dpt.collectingReason = :collectingReason " +
+  "and dpt.taxonomyCode = :taxonomyCode")
+List<DebtPositionType> findByMainFields(String code, Long brokerId, String orgType, String macroArea, String serviceType, String collectingReason, String taxonomyCode);
+
 }
