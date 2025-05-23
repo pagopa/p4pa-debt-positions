e347533add25cce0171a12fcecec22bd5b1a6734
Giuseppe-LaManna <127106061+Giuseppe-LaManna@users.noreply.github.com>
2025-05-23 16:21:27 +0200

    fix: P4ADEV-2972 updated validateOperator with organizationId (#232)

--- src/main/java/it/gov/pagopa/pu/debtpositions/repository/DebtPositionRepository.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/DebtPositionRepository.java
@@ -87,8 +87,11 @@ public interface DebtPositionRepository extends JpaRepository<DebtPosition, Long
    FROM DebtPosition d
         JOIN DebtPositionTypeOrg dpto on d.debtPositionTypeOrgId = dpto.debtPositionTypeOrgId
         JOIN DebtPositionTypeOrgOperators dptoo on d.debtPositionTypeOrgId = dptoo.debtPositionTypeOrgId
-     WHERE d.debtPositionId= :debtPositionId AND dptoo.operatorExternalUserId = :operatorExternalUserId
+     WHERE d.debtPositionId = :debtPositionId
+     AND dptoo.operatorExternalUserId = :operatorExternalUserId
+     AND d.organizationId = :organizationId
    """)
-  long validateOperator(@Parameter(required = true) @Param("debtPositionId") Long debtPositionId,
-                            @Parameter(required = true) @Param("operatorExternalUserId") String operatorExternalUserId);
+  long validateOperator(@Parameter(required = true, schema = @Schema(type = "integer", format = "int64")) @Param("debtPositionId") Long debtPositionId,
+                        @Parameter(required = true, schema = @Schema(type = "integer", format = "int64")) @Param("organizationId") Long organizationId,
+                        @Parameter(required = true) @Param("operatorExternalUserId") String operatorExternalUserId);
 }
