e3b84de2d44b5331b31da2e26ea01f1d3e074eed
Antonio Calò <antonio.calo@nttdata.com>
2025-05-26 18:35:23 +0200

    feat: P4ADEV-2981 add sourceFlowName field (#238)

--- src/main/java/it/gov/pagopa/pu/debtpositions/mapper/ReceiptWithAdditionalInfoMapper.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/mapper/ReceiptWithAdditionalInfoMapper.java
@@ -61,7 +61,7 @@ public class ReceiptWithAdditionalInfoMapper {
           .ingestionFlowFileId(null)
           .ingestionFlowFileLineNumber(null)
           .receiptId(receiptDTO.getReceiptId())
-          .sourceFlowName("UNKNOWN") //TODO task P4ADEV-2965
+          .sourceFlowName(receiptDTO.getSourceFlowName())
           .creationDate(receiptDTO.getPaymentDateTime())
           .updateDate(now)
           .transfers(receiptDTO.getTransfers().stream().map(this::mapTransfer).toList())
