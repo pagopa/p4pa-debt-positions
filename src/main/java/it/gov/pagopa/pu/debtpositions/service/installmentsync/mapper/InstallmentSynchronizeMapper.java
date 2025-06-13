9d36ae70bddea79697bb7c24dfd6736badf02d00
RiccardoGiuliani <144138935+RiccardoGiuliani@users.noreply.github.com>
2025-06-13 10:46:45 +0200

    feat: P4ADEV-2854 balance creation on installmentSynchronize (#258)

--- src/main/java/it/gov/pagopa/pu/debtpositions/service/installmentsync/mapper/InstallmentSynchronizeMapper.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/service/installmentsync/mapper/InstallmentSynchronizeMapper.java
@@ -1,6 +1,8 @@
 package it.gov.pagopa.pu.debtpositions.service.installmentsync.mapper;
 
 import it.gov.pagopa.pu.debtpositions.dto.generated.*;
+import it.gov.pagopa.pu.debtpositions.service.installmentsync.operation.InstallmentSynchronizeFetchBalanceService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,8 +11,13 @@ import java.util.stream.Collectors;
 
 @Service
 public class InstallmentSynchronizeMapper {
+  private final InstallmentSynchronizeFetchBalanceService installmentSynchronizeFetchBalanceService;
 
-  public DebtPositionDTO map2DebtPositionDTO(InstallmentSynchronizeDTO installmentSynchronizeDTO, Long debtPositionTypeOrgId) {
+  public InstallmentSynchronizeMapper(InstallmentSynchronizeFetchBalanceService installmentSynchronizeFetchBalanceService) {
+    this.installmentSynchronizeFetchBalanceService = installmentSynchronizeFetchBalanceService;
+  }
+
+  public DebtPositionDTO map2DebtPositionDTO(InstallmentSynchronizeDTO installmentSynchronizeDTO, Long debtPositionTypeOrgId, String accessToken) {
     return DebtPositionDTO.builder()
       .iupdOrg(installmentSynchronizeDTO.getIupdOrg())
       .description(installmentSynchronizeDTO.getDescription())
@@ -22,28 +29,30 @@ public class InstallmentSynchronizeMapper {
       .flagPuPagoPaPayment(installmentSynchronizeDTO.getFlagPuPagoPaPayment())
       .flagIuvVolatile(Boolean.FALSE)
       .status((Boolean.TRUE).equals(installmentSynchronizeDTO.getDraft()) ? DebtPositionStatus.DRAFT : DebtPositionStatus.UNPAID)
-      .paymentOptions(List.of(map2PaymentOptionDTO(installmentSynchronizeDTO)))
+      .paymentOptions(List.of(map2PaymentOptionDTO(installmentSynchronizeDTO, accessToken)))
       .build();
   }
 
-  public PaymentOptionDTO map2PaymentOptionDTO(InstallmentSynchronizeDTO installmentSynchronizeDTO){
+  public PaymentOptionDTO map2PaymentOptionDTO(InstallmentSynchronizeDTO installmentSynchronizeDTO, String accessToken){
     return PaymentOptionDTO.builder()
       .paymentOptionIndex(installmentSynchronizeDTO.getPaymentOptionIndex())
       .paymentOptionType(PaymentOptionDTO.PaymentOptionTypeEnum.valueOf(installmentSynchronizeDTO.getPaymentOptionType()))
       .description(installmentSynchronizeDTO.getPaymentOptionDescription())
       .status(Boolean.TRUE.equals(installmentSynchronizeDTO.getDraft()) ? PaymentOptionStatus.DRAFT :PaymentOptionStatus.UNPAID)
-      .installments(List.of(map2Installment(installmentSynchronizeDTO)))
+      .installments(List.of(map2Installment(installmentSynchronizeDTO, accessToken)))
       .build();
   }
 
-  public InstallmentDTO map2Installment(InstallmentSynchronizeDTO installmentSynchronizeDTO){
+  public InstallmentDTO map2Installment(InstallmentSynchronizeDTO installmentSynchronizeDTO, String accessToken){
+    String balance = StringUtils.isNotBlank(installmentSynchronizeDTO.getBalance()) ? installmentSynchronizeDTO.getBalance() : installmentSynchronizeFetchBalanceService.getDebtPositionTypeDefaultBalance(installmentSynchronizeDTO, accessToken);
+
     return InstallmentDTO.builder()
       .iud(installmentSynchronizeDTO.getIud())
       .iuv(installmentSynchronizeDTO.getIuv())
       .dueDate(installmentSynchronizeDTO.getDueDate())
       .amountCents(installmentSynchronizeDTO.getAmountCents())
       .remittanceInformation(installmentSynchronizeDTO.getRemittanceInformation())
-      .balance(installmentSynchronizeDTO.getBalance())
+      .balance(balance)
       .legacyPaymentMetadata(installmentSynchronizeDTO.getLegacyPaymentMetadata())
       .debtor(map2PersonDTO(installmentSynchronizeDTO))
       .ingestionFlowFileId(installmentSynchronizeDTO.getIngestionFlowFileId())
