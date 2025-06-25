0e55ae124e3c4b2c835d1336bfc05cee007c2692
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2025-06-25 15:23:38 +0200

    [P4ADEV-3260] fix installment sync for draft installments

--- src/main/java/it/gov/pagopa/pu/debtpositions/service/installmentsync/operation/InstallmentSynchronizeInsertService.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/service/installmentsync/operation/InstallmentSynchronizeInsertService.java
@@ -24,8 +24,8 @@ public class InstallmentSynchronizeInsertService extends BaseInstallmentSynchron
   private final DebtPositionAddInstallmentService debtPositionAddInstallmentService;
 
   private static final Set<InstallmentStatus> installmentStatusesValidForInsertion = Set.of(InstallmentStatus.CANCELLED, InstallmentStatus.INVALID);
-  private static final Set<PaymentOptionStatus> paymentOptionStatusesValidForInsertion = Set.of(PaymentOptionStatus.UNPAID, PaymentOptionStatus.EXPIRED, PaymentOptionStatus.PARTIALLY_PAID, PaymentOptionStatus.TO_SYNC);
-  private static final Set<DebtPositionStatus> debtPositionStatusesValidForInsertion = Set.of(DebtPositionStatus.UNPAID, DebtPositionStatus.EXPIRED, DebtPositionStatus.PARTIALLY_PAID, DebtPositionStatus.TO_SYNC);
+  private static final Set<PaymentOptionStatus> paymentOptionStatusesValidForInsertion = Set.of(PaymentOptionStatus.UNPAID, PaymentOptionStatus.EXPIRED, PaymentOptionStatus.PARTIALLY_PAID, PaymentOptionStatus.TO_SYNC, PaymentOptionStatus.DRAFT);
+  private static final Set<DebtPositionStatus> debtPositionStatusesValidForInsertion = Set.of(DebtPositionStatus.UNPAID, DebtPositionStatus.EXPIRED, DebtPositionStatus.PARTIALLY_PAID, DebtPositionStatus.TO_SYNC, DebtPositionStatus.DRAFT);
 
   public InstallmentSynchronizeInsertService(InstallmentSynchronizeApplierService installmentSynchronizeApplierService, DebtPositionCreationService debtPositionCreationService, DebtPositionAddInstallmentService debtPositionAddInstallmentService) {
     this.installmentSynchronizeApplierService = installmentSynchronizeApplierService;
