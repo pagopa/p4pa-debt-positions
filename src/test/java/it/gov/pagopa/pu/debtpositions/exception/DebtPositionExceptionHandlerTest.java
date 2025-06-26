f5483f067ef02e228388f1bacd003afe89eef274
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2025-06-26 16:52:07 +0200

    P4ADEV-2960 fix test

--- src/test/java/it/gov/pagopa/pu/debtpositions/exception/DebtPositionExceptionHandlerTest.java
+++ src/test/java/it/gov/pagopa/pu/debtpositions/exception/DebtPositionExceptionHandlerTest.java
@@ -346,7 +346,7 @@ class DebtPositionExceptionHandlerTest {
     doThrow(new InvalidParamException("Error")).when(testControllerSpy).testEndpoint(DATA, BODY);
 
     performRequest(DATA, MediaType.APPLICATION_JSON)
-      .andExpect(MockMvcResultMatchers.status().isInternalServerError())
+      .andExpect(MockMvcResultMatchers.status().isBadRequest())
       .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("DEBT_POSITION_BAD_REQUEST"))
       .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
   }
