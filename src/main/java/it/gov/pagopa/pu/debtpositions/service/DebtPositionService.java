8ee75e11c3b5c11b07f15e3c853d53f9128d1ec4
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2025-06-27 10:13:13 +0200

    [P4ADEV-3268] merge with dev

--- src/main/java/it/gov/pagopa/pu/debtpositions/service/DebtPositionService.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/service/DebtPositionService.java
@@ -1,6 +1,8 @@
 package it.gov.pagopa.pu.debtpositions.service;
 
 import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionDTO;
+import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionOrigin;
+import it.gov.pagopa.pu.debtpositions.dto.generated.InstallmentStatus;
 import it.gov.pagopa.pu.debtpositions.dto.generated.PagedDebtPositions;
 import it.gov.pagopa.pu.debtpositions.model.DebtPosition;
 import org.springframework.data.domain.Pageable;
