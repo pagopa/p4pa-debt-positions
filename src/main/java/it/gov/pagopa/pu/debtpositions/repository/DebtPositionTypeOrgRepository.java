70004ca3c84debdef8119a1661fd8a3ecfd5a869
antoniocalo <antonio.calo@nttdata.com>
2025-07-02 12:55:35 +0200

    P4ADEV-3326 add findDebtPositionTypeOrgByNavAndOrigins

--- src/main/java/it/gov/pagopa/pu/debtpositions/repository/DebtPositionTypeOrgRepository.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/DebtPositionTypeOrgRepository.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.pu.debtpositions.repository;
 
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
+import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionOrigin;
 import it.gov.pagopa.pu.debtpositions.model.DebtPositionTypeOrg;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -56,5 +57,15 @@ public interface DebtPositionTypeOrgRepository extends JpaRepository<DebtPositio
           @Parameter(required = true) @Param("code") String code,
           @Parameter(required = true) @Param("operatorExternalUserId") String operatorExternalUserId
   );
+
+  @Query("""
+      SELECT dpto from InstallmentNoPII i
+      JOIN PaymentOption po ON i.paymentOptionId = po.paymentOptionId
+      JOIN DebtPosition dp ON po.debtPositionId = dp.debtPositionId
+      JOIN DebtPositionTypeOrg dpto ON dpto.debtPositionTypeOrgId = dp.debtPositionTypeOrgId
+      WHERE i.nav = :nav
+      AND (:debtPositionOrigins IS NULL OR dp.debtPositionOrigin IN :debtPositionOrigins)
+      """)
+  DebtPositionTypeOrg findDebtPositionTypeOrgByNavAndOrigins(String nav, List<DebtPositionOrigin> debtPositionOrigins);
 }
 
