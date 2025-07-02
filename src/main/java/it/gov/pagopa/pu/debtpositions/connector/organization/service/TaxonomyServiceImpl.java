b716733803d76da096c4b0f75bec8ce66a36b2d4
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2025-07-02 12:10:44 +0200

    [P4ADEV-2379] validate taxonomy category

--- src/main/java/it/gov/pagopa/pu/debtpositions/connector/organization/service/TaxonomyServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/connector/organization/service/TaxonomyServiceImpl.java
@@ -2,14 +2,11 @@ package it.gov.pagopa.pu.debtpositions.connector.organization.service;
 
 import it.gov.pagopa.pu.debtpositions.connector.organization.client.TaxonomySearchClient;
 import it.gov.pagopa.pu.organization.dto.generated.PagedModelTaxonomy;
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@CacheConfig(cacheNames = it.gov.pagopa.pu.debtpositions.config.CacheConfig.Fields.taxonomy)
 public class TaxonomyServiceImpl implements TaxonomyService {
 
   private final TaxonomySearchClient taxonomySearchClient;
@@ -19,7 +16,6 @@ public class TaxonomyServiceImpl implements TaxonomyService {
   }
 
   @Override
-  @Cacheable(key = "#taxonomies", unless = "#result == null")
   public PagedModelTaxonomy getTaxonomies(String organizationType, String macroAreaCode,
                                           String serviceTypeCode, String collectionReason,
                                           Integer page, Integer size, List<String> sort, String accessToken) {
