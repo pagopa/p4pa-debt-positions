3d7750afb9fc7cfaafd19af271965a7289cce2a1
ElisKina-dev <elis.kina@nttdata.com>
2025-09-11 16:34:01 +0200

    deleteOperators  API REST

---
+++ src/test/java/it/gov/pagopa/pu/debtpositions/controller/DebtPositionTypeOrgOperatorsControllerTest.java
@@ -0,0 +1,48 @@
+package it.gov.pagopa.pu.debtpositions.controller;
+
+import it.gov.pagopa.pu.debtpositions.service.DebtPositionTypeOrgOperatorsService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class DebtPositionTypeOrgOperatorsControllerTest {
+  @Mock
+  private DebtPositionTypeOrgOperatorsService debtPositionTypeOrgOperatorsServiceMock;
+
+  private DebtPositionTypeOrgOperatorsController controller;
+
+  @BeforeEach
+  void setUp() {
+    controller = new DebtPositionTypeOrgOperatorsController(debtPositionTypeOrgOperatorsServiceMock);
+  }
+
+  @Test
+  void givenDebtPositionTypeOrgIdAndSetOfExternalOperatorUserIdsWhenDeleteOperatorsThenReturnDeletedCount() {
+    Long debtPositionTypeOrgId = 123L;
+    Set<String> operatorIds = Set.of("op1", "op2");
+    int expectedDeletedCount = 2;
+
+    when(debtPositionTypeOrgOperatorsServiceMock.deleteOperators(debtPositionTypeOrgId, operatorIds))
+      .thenReturn(expectedDeletedCount);
+
+    ResponseEntity<Integer> result = controller.deleteOperators(debtPositionTypeOrgId, operatorIds);
+
+    assertNotNull(result);
+    assertEquals(HttpStatus.OK, result.getStatusCode());
+    assertEquals(expectedDeletedCount, result.getBody());
+
+    verify(debtPositionTypeOrgOperatorsServiceMock, times(1))
+      .deleteOperators(debtPositionTypeOrgId, operatorIds);
+  }
+}
