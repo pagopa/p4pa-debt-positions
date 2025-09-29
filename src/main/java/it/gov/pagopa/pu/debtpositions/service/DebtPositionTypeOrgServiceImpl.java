dee16d7200c4d282fca6ff22ccd7bc1b96a64996
RiccardoGiuliani <144138935+RiccardoGiuliani@users.noreply.github.com>
2025-09-29 17:33:09 +0200

    fix: P4ADEV-3866 readonly fields debt position type org (#366)

--- src/main/java/it/gov/pagopa/pu/debtpositions/service/DebtPositionTypeOrgServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/service/DebtPositionTypeOrgServiceImpl.java
@@ -106,8 +106,6 @@ public class DebtPositionTypeOrgServiceImpl implements DebtPositionTypeOrgServic
     checkImmutableField("code", existingDebtPositionTypeOrg.getCode(), updatedDebtPositionTypeOrg.getCode(), modifiedFields);
     checkImmutableField("description", existingDebtPositionTypeOrg.getDescription(), updatedDebtPositionTypeOrg.getDescription(), modifiedFields);
     checkImmutableField("orgSector", existingDebtPositionTypeOrg.getOrgSector(), updatedDebtPositionTypeOrg.getOrgSector(), modifiedFields);
-    checkImmutableField("flagAnonymousFiscalCode", existingDebtPositionTypeOrg.isFlagAnonymousFiscalCode(), updatedDebtPositionTypeOrg.isFlagAnonymousFiscalCode(), modifiedFields);
-    checkImmutableField("flagMandatoryDueDate", existingDebtPositionTypeOrg.isFlagMandatoryDueDate(), updatedDebtPositionTypeOrg.isFlagMandatoryDueDate(), modifiedFields);
     checkImmutableField("flagActive", existingDebtPositionTypeOrg.isFlagActive(), updatedDebtPositionTypeOrg.isFlagActive(), modifiedFields);
     checkImmutableField("flagAmountActualization", existingDebtPositionTypeOrg.isFlagAmountActualization(), updatedDebtPositionTypeOrg.isFlagAmountActualization(), modifiedFields);
     checkImmutableField("flagExternal", existingDebtPositionTypeOrg.isFlagExternal(), updatedDebtPositionTypeOrg.isFlagExternal(), modifiedFields);
