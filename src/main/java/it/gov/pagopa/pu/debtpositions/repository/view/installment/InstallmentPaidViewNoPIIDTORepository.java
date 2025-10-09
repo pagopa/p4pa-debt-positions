11cba97ae8e5aaf2fb873309195f3d28946942be
pelliccm <matteo.pelliccione@nttdata.com>
2025-10-09 13:44:08 +0200

    [P4ADEV-3966] Query for PaidExportFile without operator

--- src/main/java/it/gov/pagopa/pu/debtpositions/repository/view/installment/InstallmentPaidViewNoPIIDTORepository.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/view/installment/InstallmentPaidViewNoPIIDTORepository.java
@@ -58,10 +58,62 @@ public interface InstallmentPaidViewNoPIIDTORepository extends Repository<Instal
       AND (CAST(:#{#filter.installmentUpdateDateTime.to} AS STRING) IS NULL OR i.updateDate <= :#{#filter.installmentUpdateDateTime.to})
       AND dptoo.operatorExternalUserId = :#{#filter.operatorExternalUserId}
       AND (:#{#filter.debtPositionTypeOrgId} IS NULL OR dptoo.debtPositionTypeOrgId = :#{#filter.debtPositionTypeOrgId})
+      AND (:#{#filter.debtPositionOrigins} IS NULL OR dp.debtPositionOrigin IN :#{#filter.debtPositionOrigins})
       AND t.transferIndex = 1
   """
   )
   Page<InstallmentPaidViewNoPII> findInstallmentPaidViewNoPIIDTO(
+    @Parameter(required = true) @Param("filter") ExportPaidInstallmentsFiltersDTO exportPaidInstallmentsFiltersDTO,
+    Pageable pageable
+  );
+
+  @RestResource(exported = false)
+  @Query("""
+    SELECT new InstallmentPaidViewNoPII(
+      i.installmentId as installmentId,
+      i.sourceFlowName as iuf,
+      i.iud as iud,
+      r.noticeNumber as noticeNumber,
+      dp.organizationId as organizationId,
+      r.orgFiscalCode as orgFiscalCode,
+      r.paymentReceiptId as paymentReceiptId,
+      r.paymentDateTime as paymentDateTime,
+      r.idPsp as idPsp,
+      r.pspCompanyName as pspCompanyName,
+      r.paymentAmountCents as paymentAmountCents,
+      r.creditorReferenceId as creditorReferenceId,
+      t.amountCents as amountCents,
+      t.remittanceInformation as remittanceInformation,
+      t.category as category,
+      dpto.code as code,
+      t.transferIndex as transferIndex,
+      r.feeCents as feeCents,
+      i.balance as balance,
+      r.companyName as companyName,
+      r.personalDataId as receiptPersonalDataId,
+      r.rtFilePath as rtFilePath,
+      i.iun as iun,
+      i.notificationDate as notificationDate,
+      i.notificationFeeCents as notificationFeeCents
+    )
+    FROM InstallmentNoPII i
+    JOIN PaymentOption po ON i.paymentOptionId = po.paymentOptionId
+    JOIN DebtPosition dp ON po.debtPositionId = dp.debtPositionId
+    JOIN ReceiptNoPII r ON i.receiptId = r.receiptId
+    JOIN Transfer t ON i.installmentId = t.installmentId
+    JOIN DebtPositionTypeOrg dpto ON dp.debtPositionTypeOrgId = dpto.debtPositionTypeOrgId
+    WHERE
+      i.status in (:#{T(it.gov.pagopa.pu.debtpositions.util.InstallmentUtils).PAID_STATUSES})
+      AND dp.organizationId = :#{#filter.organizationId}
+      AND (CAST(:#{#filter.paymentDateTime.from} AS STRING) IS NULL OR r.paymentDateTime >= :#{#filter.paymentDateTime.from})
+      AND (CAST(:#{#filter.paymentDateTime.to} AS STRING) IS NULL OR r.paymentDateTime <= :#{#filter.paymentDateTime.to})
+      AND (CAST(:#{#filter.installmentUpdateDateTime.from} AS STRING) IS NULL OR i.updateDate >= :#{#filter.installmentUpdateDateTime.from})
+      AND (CAST(:#{#filter.installmentUpdateDateTime.to} AS STRING) IS NULL OR i.updateDate <= :#{#filter.installmentUpdateDateTime.to})
+      AND (:#{#filter.debtPositionOrigins} IS NULL OR dp.debtPositionOrigin IN :#{#filter.debtPositionOrigins})
+      AND t.transferIndex = 1
+  """
+  )
+  Page<InstallmentPaidViewNoPII> findInstallmentPaidViewNoPIIDTOWithoutOperator(
     @Parameter(required = true) @Param("filter") ExportPaidInstallmentsFiltersDTO exportPaidInstallmentsFiltersDTO,
     Pageable pageable
   );
--- src/test/java/it/gov/pagopa/pu/debtpositions/repository/view/installment/InstallmentPaidViewPIIViewRepositoryImplTest.java
+++ src/test/java/it/gov/pagopa/pu/debtpositions/repository/view/installment/InstallmentPaidViewPIIViewRepositoryImplTest.java
@@ -1,5 +1,9 @@
 package it.gov.pagopa.pu.debtpositions.repository.view.installment;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import it.gov.pagopa.pu.debtpositions.dto.ExportPaidInstallmentsFiltersDTO;
 import it.gov.pagopa.pu.debtpositions.dto.OffsetDateTimeIntervalFilter;
 import it.gov.pagopa.pu.debtpositions.dto.generated.PagedInstallmentsPaidView;
@@ -7,6 +11,10 @@ import it.gov.pagopa.pu.debtpositions.exception.custom.ExportTooManyRecordsExcep
 import it.gov.pagopa.pu.debtpositions.mapper.PagedInstallmentsPaidViewMapper;
 import it.gov.pagopa.pu.debtpositions.model.view.installment.InstallmentPaidViewNoPII;
 import it.gov.pagopa.pu.debtpositions.util.TestUtils;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,13 +30,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.co.jemos.podam.api.PodamFactory;
-
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith({SpringExtension.class, MockitoExtension.class})
 @TestPropertySource(properties = {"data-export.installment-paid-view.max-total-elements=10"})
@@ -62,7 +63,7 @@ class InstallmentPaidViewPIIViewRepositoryImplTest {
     Long debtPositionTypeOrgId = 1L;
 
     OffsetDateTimeIntervalFilter offsetDateTimeIntervalFilter = new OffsetDateTimeIntervalFilter(paymentDateFrom, paymentDateTo);
-    ExportPaidInstallmentsFiltersDTO exportPaidInstallmentsFiltersDTO = new ExportPaidInstallmentsFiltersDTO(organizationId, operatorExternalUserId, offsetDateTimeIntervalFilter, null, debtPositionTypeOrgId );
+    ExportPaidInstallmentsFiltersDTO exportPaidInstallmentsFiltersDTO = new ExportPaidInstallmentsFiltersDTO(organizationId, operatorExternalUserId, offsetDateTimeIntervalFilter, null, debtPositionTypeOrgId, null);
     List<InstallmentPaidViewNoPII> content = podamFactory.manufacturePojo(List.class, InstallmentPaidViewNoPII.class);
     Pageable pageable = PageRequest.of(0, 10);
     Page<InstallmentPaidViewNoPII> installmentPaidViewNoPIIS = new PageImpl<>(content, pageable, 10);
@@ -98,7 +99,7 @@ class InstallmentPaidViewPIIViewRepositoryImplTest {
     Page<InstallmentPaidViewNoPII> installmentPaidViewNoPIIS = new PageImpl<>(content, pageable, 12);
 
     OffsetDateTimeIntervalFilter offsetDateTimeIntervalFilter = new OffsetDateTimeIntervalFilter(paymentDateFrom, paymentDateTo);
-    ExportPaidInstallmentsFiltersDTO exportPaidInstallmentsFiltersDTO = new ExportPaidInstallmentsFiltersDTO(organizationId, operatorExternalUserId, offsetDateTimeIntervalFilter, null, debtPositionTypeOrgId );
+    ExportPaidInstallmentsFiltersDTO exportPaidInstallmentsFiltersDTO = new ExportPaidInstallmentsFiltersDTO(organizationId, operatorExternalUserId, offsetDateTimeIntervalFilter, null, debtPositionTypeOrgId, null);
     Mockito.when(installmentPaidViewNoPIIDTORepositoryMock.findInstallmentPaidViewNoPIIDTO(exportPaidInstallmentsFiltersDTO, Pageable.ofSize(1))).thenReturn(installmentPaidViewNoPIIS);
     //when
     Executable executable = () -> installmentPaidViewPIIViewRepository.getPagedInstallmentPaidView(exportPaidInstallmentsFiltersDTO, Pageable.ofSize(1));
