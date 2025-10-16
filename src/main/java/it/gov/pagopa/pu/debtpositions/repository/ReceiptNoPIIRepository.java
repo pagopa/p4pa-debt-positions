63a6d75f4abb6036ed8d69ce0ef19a0f938bd7c2
RiccardoGiuliani <144138935+RiccardoGiuliani@users.noreply.github.com>
2025-10-16 15:36:06 +0200

    fix: P4ADEV-3974 export getByPaymentReceiptId (#399)

--- src/main/java/it/gov/pagopa/pu/debtpositions/repository/ReceiptNoPIIRepository.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/ReceiptNoPIIRepository.java
@@ -5,12 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "receipts")
 public interface ReceiptNoPIIRepository extends JpaRepository<ReceiptNoPII,Long> {
 
-  @RestResource(exported = false)
   @Query(" select r" +
     "  from ReceiptNoPII r" +
     " where r.paymentReceiptId = :paymentReceiptId")
