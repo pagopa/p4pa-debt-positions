f588bbeb20c059aa39a8592dfde464d8bc4e9298
mscarsel <marco.scarsella@nttdata.com>
2025-10-16 09:53:33 +0200

    [P4PU-1001] deleteSpontaneousForm API

--- src/main/java/it/gov/pagopa/pu/debtpositions/service/SpontaneousFormServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/service/SpontaneousFormServiceImpl.java
@@ -1,7 +1,9 @@
 package it.gov.pagopa.pu.debtpositions.service;
 
 import it.gov.pagopa.pu.debtpositions.exception.custom.ConflictErrorException;
+import it.gov.pagopa.pu.debtpositions.exception.custom.NotFoundException;
 import it.gov.pagopa.pu.debtpositions.model.SpontaneousForm;
+import it.gov.pagopa.pu.debtpositions.repository.DebtPositionTypeOrgRepository;
 import it.gov.pagopa.pu.debtpositions.repository.SpontaneousFormRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.ValidationException;
@@ -11,9 +13,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class SpontaneousFormServiceImpl implements SpontaneousFormService {
   private final SpontaneousFormRepository spontaneousFormRepository;
+  private final DebtPositionTypeOrgRepository debtPositionTypeOrgRepository;
 
-  public SpontaneousFormServiceImpl(SpontaneousFormRepository spontaneousFormRepository) {
+  public SpontaneousFormServiceImpl(SpontaneousFormRepository spontaneousFormRepository, DebtPositionTypeOrgRepository debtPositionTypeOrgRepository) {
     this.spontaneousFormRepository = spontaneousFormRepository;
+    this.debtPositionTypeOrgRepository = debtPositionTypeOrgRepository;
   }
 
   @Transactional
@@ -27,5 +31,18 @@ public class SpontaneousFormServiceImpl implements SpontaneousFormService {
       throw new ConflictErrorException("There is another SpontaneousForm with organizationId "+spontaneousForm.getOrganizationId()+" and code "+spontaneousForm.getCode());
     }
     return spontaneousFormRepository.save(spontaneousForm);
+  }
+
+  @Override
+  public void deleteSpontaneousForm(Long spontaneousFormId) {
+    SpontaneousForm spontaneousForm = spontaneousFormRepository.findById(
+        spontaneousFormId).orElseThrow(
+        () -> new NotFoundException("SpontaneousForm having id "+spontaneousFormId+" not found"));
+
+    long dptoCount = debtPositionTypeOrgRepository.countBySpontaneousFormId(spontaneousFormId);
+    if(dptoCount > 0L){
+      throw new ConflictErrorException("The SpontaneousForm having id "+spontaneousFormId+" is referenced by "+dptoCount+" DebtPositionTypeOrgs");
+    }
+    spontaneousFormRepository.delete(spontaneousForm);
   }
 }
