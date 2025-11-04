7b257c404a59aa75ce02ea5303dae2a10345d271
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2025-11-04 11:01:45 +0100

    P4PU-1013 updated query

--- src/main/java/it/gov/pagopa/pu/debtpositions/repository/view/receipt/ReceiptNoPIIViewRepository.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/view/receipt/ReceiptNoPIIViewRepository.java
@@ -38,11 +38,12 @@ public interface ReceiptNoPIIViewRepository extends Repository<ReceiptNoPIIView,
     JOIN DebtPositionType dpt ON dpto.debtPositionTypeId = dpt.debtPositionTypeId
     WHERE
       (r.debtorFiscalCodeHash = :#{@dataCipherService.hash(#debtorFiscalCode)})
+      AND dp.debtPositionOrigin in (:#{T(it.gov.pagopa.pu.debtpositions.util.InstallmentUtils).PRIMARY_ORG_DEBT_POSITION_ORIGINS})
       AND (r.orgFiscalCode IN :organizationsFiscalCode)
       AND ((:receiptOrigins IS NULL) OR (r.receiptOrigin IN :receiptOrigins))
    """
   )
-  Page<ReceiptNoPIIView> getPagedReceiptByFilters(
+  Page<ReceiptNoPIIView> getPagedPrimaryReceiptByFilters(
     @Parameter(required = true) @Param("debtorFiscalCode") String debtorFiscalCode,
     @Parameter(required = true, array = @ArraySchema(schema = @Schema(type = "string"))) @Param("organizationsFiscalCode") List<String> organizationsFiscalCode,
     @Param("receiptOrigins") List<ReceiptOriginType> receiptOrigins,
