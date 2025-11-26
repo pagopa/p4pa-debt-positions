1350a4e1242146dd7232145a30c8f59913ee367e
RiccardoGiuliani <144138935+RiccardoGiuliani@users.noreply.github.com>
2025-11-26 17:08:48 +0100

    fix: P4ADEV-4232 modified input/output validateTaxonomyCategory api (#438)

--- src/main/java/it/gov/pagopa/pu/debtpositions/service/TaxonomyValidatorService.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/service/TaxonomyValidatorService.java
@@ -1,7 +1,7 @@
 package it.gov.pagopa.pu.debtpositions.service;
 
 public interface TaxonomyValidatorService {
-  void validateTaxonomyCategory(String taxonomyCategory);
+  boolean validateTaxonomyCategory(String taxonomyCategory, String orgFiscalCode);
   boolean isTaxonomyCodeValid(String taxonomyCode, String orgTypeCode);
   boolean isTaxonomyCategoryValid(String taxonomyCategory, String orgTypeCode);
 }
