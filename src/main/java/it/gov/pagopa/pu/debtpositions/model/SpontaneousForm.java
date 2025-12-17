dc9eb9c6d87a7798d73e702e36696b7a9fc25567
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2025-12-17 12:47:13 +0100

    P4PU-1072 fixed query

--- src/main/java/it/gov/pagopa/pu/debtpositions/model/SpontaneousForm.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/model/SpontaneousForm.java
@@ -42,6 +42,7 @@ public class SpontaneousForm extends BaseEntity implements Serializable {
     (SELECT COUNT(*)
       FROM debt_position_type_org dpto
       WHERE organization_id = dpto.organization_id
+      AND spontaneous_form_id = dpto.spontaneous_form_id
       AND dpto.flag_active = true)
     """)
   private Integer debtPositionTypeOrgCount;
