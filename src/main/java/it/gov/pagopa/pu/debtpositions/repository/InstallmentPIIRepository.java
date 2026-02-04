eddb9dab9556708f4543843d0157d7877cecfbcd
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2026-02-04 10:44:25 +0100

    P4PU-1128 Add optional installment status filter for installments API

--- src/main/java/it/gov/pagopa/pu/debtpositions/repository/InstallmentPIIRepository.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/repository/InstallmentPIIRepository.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.pu.debtpositions.repository;
 
 import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionOrigin;
 import it.gov.pagopa.pu.debtpositions.dto.generated.InstallmentDTO;
+import it.gov.pagopa.pu.debtpositions.dto.generated.InstallmentStatus;
 import it.gov.pagopa.pu.debtpositions.model.InstallmentNoPII;
 
 import java.util.List;
@@ -16,5 +17,5 @@ public interface InstallmentPIIRepository {
 
   List<InstallmentDTO> getByOrganizationIdAndReceiptId(Long organizationId, Long receiptId, List<DebtPositionOrigin> debtPositionOrigin);
 
-  List<InstallmentDTO> findByIuvOrNav(String iuvOrNav, String debtorFiscalCode, Long organizationId);
+  List<InstallmentDTO> findByIuvOrNav(String iuvOrNav, String debtorFiscalCode, Long organizationId, List<InstallmentStatus> statuses);
 }
