6593368c45d35461098f3bc1443dc6d6dcccecc6
michele.mazzini <michele.mazzini1996@gmail.com>
2026-02-05 10:18:32 +0100

    fix: remove unused imports

--- src/test/java/it/gov/pagopa/pu/debtpositions/service/create/receipt/primaryorg/PrimaryOrgPaymentHandlerServiceTest.java
+++ src/test/java/it/gov/pagopa/pu/debtpositions/service/create/receipt/primaryorg/PrimaryOrgPaymentHandlerServiceTest.java
@@ -1,6 +1,5 @@
 package it.gov.pagopa.pu.debtpositions.service.create.receipt.primaryorg;
 
-import it.gov.pagopa.pu.debtpositions.connector.organization.service.OrganizationService;
 import it.gov.pagopa.pu.debtpositions.dto.generated.ReceiptWithAdditionalNodeDataDTO;
 import it.gov.pagopa.pu.debtpositions.model.DebtPosition;
 import it.gov.pagopa.pu.debtpositions.model.InstallmentNoPII;
