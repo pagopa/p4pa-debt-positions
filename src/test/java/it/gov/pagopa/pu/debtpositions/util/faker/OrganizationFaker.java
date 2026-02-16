de7972a23929d6ca2a229ed970f894f0dd3f3b87
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2026-02-16 12:32:56 +0100

    [P4ADEV-4488] add flag owner + delegate logic

--- src/test/java/it/gov/pagopa/pu/debtpositions/util/faker/OrganizationFaker.java
+++ src/test/java/it/gov/pagopa/pu/debtpositions/util/faker/OrganizationFaker.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.pu.debtpositions.util.faker;
 
 import it.gov.pagopa.pu.debtpositions.util.TestUtils;
+import it.gov.pagopa.pu.organization.dto.generated.Broker;
 import it.gov.pagopa.pu.organization.dto.generated.Organization;
 import it.gov.pagopa.pu.organization.dto.generated.OrganizationStatus;
 
@@ -9,6 +10,13 @@ public class OrganizationFaker {
   public static Organization buildOrganization(){
     return TestUtils.getPodamFactory().manufacturePojo(Organization.class)
       .orgFiscalCode("12345678901")
-      .status(OrganizationStatus.ACTIVE);
+      .status(OrganizationStatus.ACTIVE)
+      .brokerId(1L);
+  }
+
+  public static Broker buildBroker(){
+    return TestUtils.getPodamFactory().manufacturePojo(Broker.class)
+      .brokerId(1L)
+      .flagDelegate(Boolean.FALSE);
   }
 }
