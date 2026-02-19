c3f28a4aca8237928078aa88c74a9365625a0fed
antonio.torre <antonio.torre@pagopa.it>
2026-02-19 13:56:18 +0100

    fix: Caching Broker, Improving AESUtils memory usage

--- src/main/java/it/gov/pagopa/pu/debtpositions/connector/organization/service/BrokerServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/connector/organization/service/BrokerServiceImpl.java
@@ -3,11 +3,14 @@ package it.gov.pagopa.pu.debtpositions.connector.organization.service;
 import it.gov.pagopa.pu.debtpositions.connector.organization.client.BrokerClient;
 import it.gov.pagopa.pu.debtpositions.connector.organization.client.BrokerSearchClient;
 import it.gov.pagopa.pu.organization.dto.generated.Broker;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@CacheConfig(cacheNames = it.gov.pagopa.pu.debtpositions.config.CacheConfig.Fields.broker)
 public class BrokerServiceImpl implements BrokerService {
 
   private final BrokerSearchClient brokerSearchClient;
@@ -26,6 +29,7 @@ public class BrokerServiceImpl implements BrokerService {
   }
 
   @Override
+  @Cacheable(key = "'brokerId-' + #brokerId", unless = "#result == null")
   public Broker findById(Long brokerId, String accessToken) {
     return brokerClient.findById(brokerId, accessToken);
   }
