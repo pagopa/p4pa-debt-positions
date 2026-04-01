407e456a609981ac26a92fe18247e3bf38ddc5fd
michele.mazzini <michele.mazzini1996@gmail.com>
2026-04-01 12:29:51 +0200

    fix: handle null postalIbans

--- src/main/java/it/gov/pagopa/pu/debtpositions/controller/DebtPositionControllerImpl.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/controller/DebtPositionControllerImpl.java
@@ -318,20 +318,12 @@ public class DebtPositionControllerImpl implements DebtPositionApi {
 
     String accessToken = SecurityUtils.getAccessToken();
 
-    String oldPostalIban = updateTransferIbansAndSyncDebtPositionRequestDTO.getOldPostalIban() != null ?
-      updateTransferIbansAndSyncDebtPositionRequestDTO.getOldPostalIban().get()
-      : null;
-
-    String newPostalIban = updateTransferIbansAndSyncDebtPositionRequestDTO.getNewPostalIban() != null ?
-      updateTransferIbansAndSyncDebtPositionRequestDTO.getNewPostalIban().get()
-      : null;
-
     massiveUpdateService.updateTransferIbansAndSyncDebtPosition(
       debtPositionId,
       updateTransferIbansAndSyncDebtPositionRequestDTO.getOldIban(),
       updateTransferIbansAndSyncDebtPositionRequestDTO.getNewIban(),
-      oldPostalIban,
-      newPostalIban,
+      updateTransferIbansAndSyncDebtPositionRequestDTO.getOldPostalIban(),
+      updateTransferIbansAndSyncDebtPositionRequestDTO.getNewPostalIban(),
       accessToken
     );
 
