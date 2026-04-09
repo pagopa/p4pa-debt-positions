b7b03495ea88825b0d22e968cd89b3ff04b2e26e
antonio.torre <antonio.torre@pagopa.it>
2026-04-09 15:41:06 +0200

    fix: Fix CVE

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,15 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
   java
-  id("org.springframework.boot") version "4.0.3"
+  id("org.springframework.boot") version "4.0.5"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
   id("org.sonarqube") version "7.2.3.7755"
   id("com.github.ben-manes.versions") version "0.53.0"
-  id("org.openapi.generator") version "7.20.0"
+  id("org.openapi.generator") version "7.21.0"
   id("org.ajoberstar.grgit") version "5.3.2"
   id("com.gorylenko.gradle-git-properties") version "2.5.7"
-  id("com.github.jk1.dependency-license-report") version "3.1.1"
+  id("com.github.jk1.dependency-license-report") version "3.1.2"
 }
 
 group = "it.gov.pagopa.payhub"
@@ -53,27 +53,27 @@ repositories {
 
 val springDocOpenApiVersion = "3.0.2"
 val janinoVersion = "3.1.12"
-val openApiToolsVersion = "0.2.9"
-val springWolfAsyncApiVersion = "2.1.0"
-val micrometerVersion = "1.6.3"
+val openApiToolsVersion = "0.2.10"
+val springWolfAsyncApiVersion = "1.20.0"
+val springWolfUiAsyncApiVersion = "1.20.0"
+val micrometerVersion = "1.6.4"
 val postgresJdbcVersion = "42.7.10"
 val bouncycastleVersion = "1.83"
 val mapStructVersion = "1.6.3"
 val podamVersion = "8.0.2.RELEASE"
 val caffeineVersion = "3.2.3"
 val httpClientVersion = "5.6"
-val httpCoreVersion = "5.4.1"
+val httpCoreVersion = "5.4.2"
 val openHtmlToPdfVersion = "1.0.10"
 val commonsLang3Version = "3.20.0"
 val lz4JavaVersion = "1.10.4"
-val itextVersion = "8.0.5"
+val itextVersion = "9.6.0"
 
 // Downgrading in order to handle List of enums in SpringDataRest exposed queries
 val hibernateCoreVersion = "7.1.18.Final"
 
 // fix cve
-val jackson2CoreVersion = "2.21.1"
-val jackson3CoreVersion = "3.1.0"
+val jackson3CoreVersion = "3.1.1"
 
 val springCloudDepsVersion = "2025.1.1"
 
@@ -125,7 +125,6 @@ dependencies {
 
   // CVE fix
   implementation("tools.jackson.core:jackson-core:$jackson3CoreVersion")
-  implementation("com.fasterxml.jackson.core:jackson-core:$jackson2CoreVersion")
 
   compileOnly("org.projectlombok:lombok")
 
