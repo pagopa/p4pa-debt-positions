80ee0a5c2d9874f26afd4a7f774dc9ac26bc07d9
Giuseppe-LaManna <127106061+Giuseppe-LaManna@users.noreply.github.com>
2026-04-15 15:26:10 +0200

    feat: P4PU-1351 add api to verify transfer postal iban (#571)

--- build.gradle.kts
+++ build.gradle.kts
@@ -228,7 +228,7 @@ openApiGenerate {
       "BaseDebtPosition" to "it.gov.pagopa.pu.debtpositions.dto.BaseDebtPosition",
       "DebtorDebtPositionDTO" to "it.gov.pagopa.pu.debtpositions.dto.DebtorDebtPositionDTO",
       "PaymentOptionType" to "it.gov.pagopa.pu.debtpositions.enums.PaymentOptionType",
-      "InstallmentViewDTO" to "it.gov.pagopa.pu.debtpositions.dto.view.InstallmentViewDTO",
+      "InstallmentViewDTO" to "it.gov.pagopa.pu.debtpositions.dto.view.InstallmentViewDTO"
     )
   )
   configOptions.set(
@@ -245,6 +245,7 @@ openApiGenerate {
       "additionalModelTypeAnnotations" to "@lombok.experimental.SuperBuilder(toBuilder = true)"
     )
   )
+
 }
 
 var targetEnv = when (Objects.requireNonNullElse(
---
+++ src/test/java/it/gov/pagopa/pu/debtpositions/service/TransferServiceImplTest.java
@@ -0,0 +1,116 @@
+package it.gov.pagopa.pu.debtpositions.service;
+
+import it.gov.pagopa.pu.debtpositions.dto.generated.PostalIbanVerifyResponse;
+import it.gov.pagopa.pu.debtpositions.exception.custom.NotFoundException;
+import it.gov.pagopa.pu.debtpositions.mapper.PostalIbanVerifyResponseMapper;
+import it.gov.pagopa.pu.debtpositions.repository.InstallmentNoPIIRepository;
+import it.gov.pagopa.pu.debtpositions.repository.TransferRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class TransferServiceImplTest {
+
+  @Mock
+  private TransferRepository transferRepositoryMock;
+  @Mock
+  private InstallmentNoPIIRepository installmentNoPIIRepositoryMock;
+  @Mock
+  private PostalIbanVerifyResponseMapper postalIbanVerifyResponseMapperMock;
+
+  private TransferServiceImpl transferService;
+
+  @BeforeEach
+  void setUp() {
+    transferService = new TransferServiceImpl(
+      transferRepositoryMock,
+      installmentNoPIIRepositoryMock,
+      postalIbanVerifyResponseMapperMock
+    );
+  }
+
+
+  @AfterEach
+  void verifyNoMoreInteractions(){
+    Mockito.verifyNoMoreInteractions(
+      transferRepositoryMock,
+      installmentNoPIIRepositoryMock,
+      postalIbanVerifyResponseMapperMock
+    );
+  }
+
+  @Test
+  void givenValidInstallmentIdsWhenVerifyPostalIbanThenReturnMappedResponse() {
+    // given
+    List<Long> installmentIds = List.of(1L, 2L, 3L);
+    Set<Long> idsWithNull = Set.of(2L);
+
+    PostalIbanVerifyResponse expectedResponse = new PostalIbanVerifyResponse();
+
+    Mockito.when(installmentNoPIIRepositoryMock.findExistingInstallmentIds(installmentIds))
+      .thenReturn(Set.of(1L, 2L, 3L));
+
+    Mockito.when(transferRepositoryMock.findInstallmentIdsWithNullPostalIban(installmentIds))
+      .thenReturn(idsWithNull);
+
+    Mockito.when(postalIbanVerifyResponseMapperMock.map(installmentIds, idsWithNull))
+      .thenReturn(expectedResponse);
+
+    // when
+    PostalIbanVerifyResponse result = transferService.verifyPostalIban(installmentIds);
+
+    // then
+    assertNotNull(result);
+    assertEquals(expectedResponse, result);
+  }
+
+  @Test
+  void givenMissingInstallmentIdsWhenVerifyPostalIbanThenThrowNotFoundException() {
+    // given
+    List<Long> installmentIds = List.of(1L, 2L, 3L);
+
+    Mockito.when(installmentNoPIIRepositoryMock.findExistingInstallmentIds(installmentIds))
+      .thenReturn(Set.of(1L, 2L));
+
+    // when & then
+    NotFoundException ex = assertThrows(NotFoundException.class,
+      () -> transferService.verifyPostalIban(installmentIds));
+
+    assertTrue(ex.getMessage().contains("3"));
+
+    Mockito.verifyNoInteractions(transferRepositoryMock, postalIbanVerifyResponseMapperMock);
+  }
+
+  @Test
+  void givenAllInstallmentsValidWhenVerifyPostalIbanThenMapperCalledWithEmptySet() {
+    // given
+    List<Long> installmentIds = List.of(1L, 2L);
+
+    PostalIbanVerifyResponse expectedResponse = new PostalIbanVerifyResponse();
+
+    Mockito.when(installmentNoPIIRepositoryMock.findExistingInstallmentIds(installmentIds))
+      .thenReturn(Set.of(1L, 2L));
+
+    Mockito.when(transferRepositoryMock.findInstallmentIdsWithNullPostalIban(installmentIds))
+      .thenReturn(Set.of());
+
+    Mockito.when(postalIbanVerifyResponseMapperMock.map(installmentIds, Set.of()))
+      .thenReturn(expectedResponse);
+
+    // when
+    PostalIbanVerifyResponse result = transferService.verifyPostalIban(installmentIds);
+
+    // then
+    assertEquals(expectedResponse, result);
+  }
+}
