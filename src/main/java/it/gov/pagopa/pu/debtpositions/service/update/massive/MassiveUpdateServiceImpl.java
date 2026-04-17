165d1c8ed1b94755d5ad4e4120ce7757f6a1e143
michele.mazzini <michele.mazzini1996@gmail.com>
2026-04-17 09:46:18 +0200

    fix: revert

--- src/main/java/it/gov/pagopa/pu/debtpositions/service/update/massive/MassiveUpdateServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/service/update/massive/MassiveUpdateServiceImpl.java
@@ -60,10 +60,8 @@ public class MassiveUpdateServiceImpl implements MassiveUpdateService {
                 // Not use InstallmentUtils for TO_SYNC status to preserve syncStatusFrom/to
                 if (!InstallmentStatus.TO_SYNC.equals(i.getStatus())) {
                   InstallmentUtils.setStatus(i, i.getStatus());
-                }
-
-                if (InstallmentStatus.TO_SYNC.equals(i.getStatus())) {
-                  i.getSyncStatus().setSyncError(null);
+                } else {
+                 i.getSyncStatus().setSyncError(null);
                 }
 
                 collectedIuds.add(i.getIud());
