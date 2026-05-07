21f6c0747bdbda2bb28322dfa420b52b7c96fc39
Leonardo Pierangelini <leonardo.pierangelini@nttdata.com>
2026-05-07 11:50:54 +0200

    P4ADEV-4646-fix-managment-of-unknown-station-id-on-technical-debt-position: fixed management of UNKNOWN station id for technical debt position; fixed typo in ERROR_CODE_ORGANIZATION_STATION_NOT_FOUND value

--- src/main/java/it/gov/pagopa/pu/debtpositions/connector/organization/service/OrganizationStationRetrieverServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/debtpositions/connector/organization/service/OrganizationStationRetrieverServiceImpl.java
@@ -5,6 +5,8 @@ import it.gov.pagopa.pu.debtpositions.util.ErrorCodeConstants;
 import it.gov.pagopa.pu.organization.dto.generated.OrganizationStationDTO;
 import org.springframework.stereotype.Service;
 
+import static it.gov.pagopa.pu.debtpositions.util.Constants.UNKNOWN_STATION_ID;
+
 @Service
 public class OrganizationStationRetrieverServiceImpl implements OrganizationStationRetrieverService {
 
@@ -16,6 +18,12 @@ public class OrganizationStationRetrieverServiceImpl implements OrganizationStat
 
   @Override
   public OrganizationStationDTO getOrganizationStation(Long organizationId, String stationId, String accessToken) {
+    if(UNKNOWN_STATION_ID.equalsIgnoreCase(stationId)) {
+      OrganizationStationDTO organizationStationDTO = new OrganizationStationDTO();
+      organizationStationDTO.setStationId(stationId);
+      organizationStationDTO.setOrganizationId(organizationId);
+      return organizationStationDTO;
+    }
     return organizationService.getOrganizationStation(organizationId, stationId, accessToken)
       .orElseThrow(() -> new NotFoundException(
         ErrorCodeConstants.ERROR_CODE_ORGANIZATION_STATION_NOT_FOUND,
