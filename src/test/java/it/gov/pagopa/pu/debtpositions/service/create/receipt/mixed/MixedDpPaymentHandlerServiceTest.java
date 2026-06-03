0f580d11d4c5724b5e8542086e0c980ed86f0a1e
michele.mazzini <michele.mazzini1996@gmail.com>
2026-06-03 12:35:25 +0200

    fix: remove unused imports

--- src/test/java/it/gov/pagopa/pu/debtpositions/service/create/receipt/mixed/MixedDpPaymentHandlerServiceTest.java
+++ src/test/java/it/gov/pagopa/pu/debtpositions/service/create/receipt/mixed/MixedDpPaymentHandlerServiceTest.java
@@ -13,7 +13,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.OffsetDateTime;
 import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
