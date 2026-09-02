c88a03e3d58de0e5dc3ac6a703505af0facd3ded
antonio.torre <antonio.torre@pagopa.it>
2026-09-02 10:00:57 +0200

    fix: Fix CVE

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,12 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
   java
-  id("org.springframework.boot") version "4.1.0"
+  id("org.springframework.boot") version "4.1.1"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
-  id("org.sonarqube") version "7.3.1.8318"
+  id("org.sonarqube") version "7.4.0.8496"
   id("com.github.ben-manes.versions") version "0.54.0"
-  id("org.openapi.generator") version "7.23.0"
+  id("org.openapi.generator") version "7.25.0"
   id("org.ajoberstar.grgit") version "5.3.2"
   id("com.gorylenko.gradle-git-properties") version "4.0.1"
   id("com.github.jk1.dependency-license-report") version "3.1.4"
@@ -51,28 +51,28 @@ repositories {
   mavenCentral()
 }
 
-val springDocOpenApiVersion = "3.0.3"
-val openApiToolsVersion = "0.2.10"
+val springDocOpenApiVersion = "3.1.0"
+val openApiToolsVersion = "0.2.11"
 val springWolfAsyncApiVersion = "1.21.0"
 val springWolfUiAsyncApiVersion = "1.21.0"
-val micrometerVersion = "1.7.0"
+val micrometerVersion = "1.7.1"
 val postgresJdbcVersion = "42.7.13"
-val bouncycastleVersion = "1.84"
+val bouncycastleVersion = "1.85.2"
 val mapStructVersion = "1.6.3"
 val podamVersion = "8.0.2.RELEASE"
 val caffeineVersion = "3.2.4"
 val httpClientVersion = "5.6.4"
 val httpCoreVersion = "5.4.3"
 val kafkaAppender = "0.2.0-RC2"
-val lz4JavaVersion = "1.11.1"
+val lz4JavaVersion = "1.11.2"
 val openHtmlToPdfVersion = "1.0.10"
 val commonsLang3Version = "3.20.0"
-val itextVersion = "9.6.0"
+val itextVersion = "9.7.1"
 
 // Downgrading in order to handle List of enums in SpringDataRest exposed queries (see https://github.com/spring-projects/spring-data-commons/issues/3502)
 val hibernateCoreVersion = "7.1.18.Final"
 
-val springCloudDepsVersion = "2025.1.2"
+val springCloudDepsVersion = "2025.1.3"
 
 dependencyManagement {
   imports {
@@ -103,11 +103,11 @@ dependencies {
     exclude(group = "org.apache.commons", module = "commons-lang3")
   }
   implementation("org.apache.commons:commons-lang3:$commonsLang3Version")
-  implementation("io.github.springwolf:springwolf-kafka:${springWolfAsyncApiVersion}") {
+  implementation("io.github.springwolf:springwolf-kafka:$springWolfAsyncApiVersion") {
     exclude(group = "org.lz4", module = "lz4-java")
   }
-  implementation("io.github.springwolf:springwolf-ui:${springWolfUiAsyncApiVersion}")
-  implementation("io.github.springwolf:springwolf-cloud-stream:${springWolfAsyncApiVersion}")
+  implementation("io.github.springwolf:springwolf-ui:$springWolfUiAsyncApiVersion")
+  implementation("io.github.springwolf:springwolf-cloud-stream:$springWolfAsyncApiVersion")
   implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
   implementation("org.mapstruct:mapstruct:$mapStructVersion")
   implementation("org.bouncycastle:bcprov-jdk18on:$bouncycastleVersion")
@@ -119,9 +119,9 @@ dependencies {
     exclude(group = "org.lz4", module = "lz4-java")
   }
   implementation("org.springframework.boot:spring-boot-starter-freemarker")
-  implementation("com.openhtmltopdf:openhtmltopdf-pdfbox:${openHtmlToPdfVersion}")
-  implementation("com.itextpdf:kernel:${itextVersion}")
-  implementation("com.itextpdf:barcodes:${itextVersion}")
+  implementation("com.openhtmltopdf:openhtmltopdf-pdfbox:$openHtmlToPdfVersion")
+  implementation("com.itextpdf:kernel:$itextVersion")
+  implementation("com.itextpdf:barcodes:$itextVersion")
 
   compileOnly("org.projectlombok:lombok")
 
